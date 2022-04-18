96ec85242f0e245ef5d51751308427955ef45fa2
chkim <cndghks0213@gmail.com>
2022-04-19 00:05:49 +0900

    프로젝트 생성

---
+++ src/main/java/numble/team4/shortformserver/ShortFormServerApplication.java
@@ -0,0 +1,13 @@
+package numble.team4.shortformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortFormServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortFormServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/numble/team4/shortformserver/ShortFormServerApplicationTests.java
@@ -0,0 +1,13 @@
+package numble.team4.shortformserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortFormServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
