2cd014e23b6903642c316fd2f7b2c79152643ee0
chkim <cndghks0213@gmail.com>
2022-04-19 00:49:07 +0900

    feat: web mvc cors 설정

---
+++ src/main/java/numble/team4/shortformserver/common/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package numble.team4.shortformserver.common.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
