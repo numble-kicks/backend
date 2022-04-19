e74fd049746c143308ef45cb33d8892db682889b
seongho-joo <hazzyseong@gmail.com>
2022-04-19 20:56:07 +0900

    test: AWS S3 upload test

--- src/main/java/numble/team4/shortformserver/ShortFormServerApplication.java
+++ src/main/java/numble/team4/shortformserver/ShortFormServerApplication.java
@@ -2,12 +2,21 @@ package numble.team4.shortformserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ShortFormServerApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+		+ "classpath:application.yml,"
+		+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(ShortFormServerApplication.class, args);
+		new SpringApplicationBuilder(ShortFormServerApplication.class)
+			.properties(APPLICATION_LOCATIONS)
+			.run(args);
 	}
 
 }
