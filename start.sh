3a5a9dd409574a426e6cee394614776b22ff1986
서재희 <50989437+jaeesu@users.noreply.github.com>
2022-04-20 16:58:03 +0900

    chore: 배포 후 애플리케이션 실행 전처리 스크립트 추가

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "> build : short-form-server-0.0.1-SNAPSHOT.jar" >> /home/ubuntu/deploy.log
+
+echo "> current running process pid 확인" >> /home/ubuntu/deploy.log
+CURRENT_PID=$(pgrep -f Amattang-0.0.1-SNAPSHOT.jar)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> not exist short form server application process - no exit" >> /home/ubuntu/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
