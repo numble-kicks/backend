8bb268050f5f55ea98682999ae5244f2c1f02aed
jaeesu <wogmlsu@gmail.com>
2022-04-27 18:33:17 +0900

    feat: 요청된 팔로우 생성 service 추가

--- src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
+++ src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
@@ -3,9 +3,4 @@ package numble.team4.shortformserver.follow.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
-<<<<<<< HEAD
-
-    boolean existsByFromMember_IdAndToMember_Id(Long fromMember, Long toMember);
-=======
->>>>>>> 8ce1bb9 (feat: follow 도메인 생성)
 }
