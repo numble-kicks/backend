9b7a22e22deff41f561fa216ee6c084f7e86fd3f
jaeesu <wogmlsu@gmail.com>
2022-04-27 14:18:33 +0900

    feat: follow 도메인 생성

--- src/main/java/numble/team4/shortformserver/follow/domain/Follow.java
+++ src/main/java/numble/team4/shortformserver/follow/domain/Follow.java
@@ -1,20 +1,33 @@
 package numble.team4.shortformserver.follow.domain;
 
+<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import numble.team4.shortformserver.follow.exception.NotSelfFollowableException;
+=======
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+>>>>>>> 8ce1bb9 (feat: follow 도메인 생성)
 import numble.team4.shortformserver.member.member.domain.Member;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
+<<<<<<< HEAD
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PROTECTED)
+=======
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+>>>>>>> 8ce1bb9 (feat: follow 도메인 생성)
 public class Follow {
 
     @Id
@@ -28,6 +41,7 @@ public class Follow {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "to_member", nullable = false)
     private Member toMember;
+<<<<<<< HEAD
 
     public static Follow fromMembers(Member fromMember, Member toMember) {
         if (fromMember.isEqualMember(toMember)) {
@@ -35,4 +49,6 @@ public class Follow {
         }
         return new Follow(null, fromMember, toMember);
     }
+=======
+>>>>>>> 8ce1bb9 (feat: follow 도메인 생성)
 }
--- src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
+++ src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
@@ -3,6 +3,9 @@ package numble.team4.shortformserver.follow.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+<<<<<<< HEAD
 
     boolean existsByFromMember_IdAndToMember_Id(Long fromMember, Long toMember);
+=======
+>>>>>>> 8ce1bb9 (feat: follow 도메인 생성)
 }
