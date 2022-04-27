52cb5c3c4e57b5bfd537acaefda481620eef2064
jaeesu <wogmlsu@gmail.com>
2022-04-27 16:01:41 +0900

    test: follow 레포지토리 테스트 추가

--- src/main/java/numble/team4/shortformserver/member/member/domain/Member.java
+++ src/main/java/numble/team4/shortformserver/member/member/domain/Member.java
@@ -1,38 +1,28 @@
 package numble.team4.shortformserver.member.member.domain;
 
-import static javax.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import numble.team4.shortformserver.common.domain.BaseTimeEntity;
-import numble.team4.shortformserver.video.domain.Video;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
-@Builder
 @NoArgsConstructor(access = PROTECTED)
-@AllArgsConstructor(access = PROTECTED)
 public class Member extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String email;
     private String name;
     private LocalDateTime lastLoginDate;
     private String profileImageUrl;
     private boolean emailVerified;
-
-    @OneToMany(mappedBy = "member")
-    private List<Video> videos = new ArrayList<>();
 }
