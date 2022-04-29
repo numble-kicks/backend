ad4eaf26af4a8253773295bbe9476a0c9c2af7a6
seongho-joo <hazzyseong@gmail.com>
2022-04-29 14:41:18 +0900

    feat: video dto 필드 추가

--- src/main/java/numble/team4/shortformserver/video/application/VideoService.java
+++ src/main/java/numble/team4/shortformserver/video/application/VideoService.java
@@ -12,7 +12,6 @@ import numble.team4.shortformserver.video.domain.Video;
 import numble.team4.shortformserver.video.domain.VideoRepository;
 import numble.team4.shortformserver.video.dto.VideoRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
@@ -23,15 +22,15 @@ public class VideoService {
     private final MemberRepository memberRepository;
     private final AmazonS3Uploader amazonS3Uploader;
 
-    public Video upload(VideoRequest request, Member member, MultipartFile videos, MultipartFile thumbnail)
+    public Video upload(VideoRequest videoRequest, Member member)
         throws IOException {
         Member findMember = memberRepository.findById(member.getId())
             .orElseThrow(NotExistMemberException::new);
 
-        S3UploadDto videoDto = amazonS3Uploader.saveToS3(videos, "video");
-        S3UploadDto thumbnailDto = amazonS3Uploader.saveToS3(thumbnail, "video/thumbnail");
+        S3UploadDto videoDto = amazonS3Uploader.saveToS3(videoRequest.getVideo(), "video");
+        S3UploadDto thumbnailDto = amazonS3Uploader.saveToS3(videoRequest.getThumbnail(), "video/thumbnail");
 
-        Video video = request.toVideo(videoDto.getFileUrl(), thumbnailDto.getFileUrl(), findMember);
+        Video video = videoRequest.toVideo(videoDto.getFileUrl(), thumbnailDto.getFileUrl(), findMember);
 
         return videoRepository.save(video);
     }
--- src/main/java/numble/team4/shortformserver/video/dto/VideoRequest.java
+++ src/main/java/numble/team4/shortformserver/video/dto/VideoRequest.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.video.domain.Video;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @AllArgsConstructor(access = PRIVATE)
@@ -15,18 +16,20 @@ import numble.team4.shortformserver.video.domain.Video;
 @Builder
 public class VideoRequest {
 
+    private MultipartFile video;
+    private MultipartFile thumbnail;
+    private String category;
     private String title;
     private String description;
 
     public Video toVideo(String videoUrl, String thumbnailUrl, Member member) {
-        return Video
-            .builder()
+        return Video.builder()
             .title(title)
             .description(description)
             .videoUrl(videoUrl)
+            .thumbnailUrl(thumbnailUrl)
             .likeCount(0L)
             .viewCount(0L)
-            .thumbnailUrl(thumbnailUrl)
             .member(member)
             .build();
     }
