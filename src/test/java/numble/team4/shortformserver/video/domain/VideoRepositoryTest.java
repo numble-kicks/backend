a2e0d79c91a02b9461fd3d2014ba7eed5ca5a1e9
seongho-joo <hazzyseong@gmail.com>
2022-04-29 12:04:09 +0900

    test: video repository 저장 테스트

---
+++ src/test/java/numble/team4/shortformserver/video/domain/VideoRepositoryTest.java
@@ -0,0 +1,62 @@
+package numble.team4.shortformserver.video.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import numble.team4.shortformserver.member.member.domain.Member;
+import numble.team4.shortformserver.member.member.domain.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("local")
+@DataJpaTest
+class VideoRepositoryTest {
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member member;
+
+    private static final String VIDEO_URL = "https://d659rm6fgd091.cloudfront.net/test.mov";
+    private static final String THUMBNAIL_URL = "https://d659rm6fgd091.cloudfront.net/test.png";
+
+    @BeforeEach
+    void setUp() {
+        member = Member.builder()
+            .videos(new ArrayList<>())
+            .build();
+    }
+
+    @Test
+    @DisplayName("video 저장 - 성공")
+    public void saveVideo() throws Exception {
+        // given
+        memberRepository.save(member);
+
+        Video video = Video.builder()
+            .title("Title")
+            .description("description")
+            .member(member)
+            .videoUrl(VIDEO_URL)
+            .thumbnailUrl(THUMBNAIL_URL)
+            .likeCount(0L)
+            .viewCount(0L)
+            .build();
+
+        // when
+        Video savedVideo = videoRepository.save(video);
+
+        // then
+        assertThat(savedVideo.getId()).isNotNull();
+        assertThat(savedVideo).isEqualTo(video);
+        assertThat(savedVideo.getCreateAt()).isNotNull();
+        assertThat(savedVideo.getModifiedAt()).isNotNull();
+    }
+}
