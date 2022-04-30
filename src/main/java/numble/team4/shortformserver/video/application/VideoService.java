72995719c3015e1d7d81ff015cf94383607c8e33
seongho-joo <hazzyseong@gmail.com>
2022-05-01 00:20:25 +0900

    feat: 비디오 서비스 수정 기능 구현 * refactor: 중복코드 메서드 작성 * feat: 작성자를 확인하는 메서드 추가 * style: VideoService 업로드 메서드명 변경

--- src/main/java/numble/team4/shortformserver/video/application/VideoService.java
+++ src/main/java/numble/team4/shortformserver/video/application/VideoService.java
@@ -7,10 +7,13 @@ import numble.team4.shortformserver.aws.application.AmazonS3Uploader;
 import numble.team4.shortformserver.aws.dto.S3UploadDto;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.MemberRepository;
+import numble.team4.shortformserver.member.member.exception.NotAuthorException;
 import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
 import numble.team4.shortformserver.video.domain.Video;
 import numble.team4.shortformserver.video.domain.VideoRepository;
 import numble.team4.shortformserver.video.dto.VideoRequest;
+import numble.team4.shortformserver.video.dto.VideoUpdateRequest;
+import numble.team4.shortformserver.video.exception.NotExistVideoException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -22,16 +25,41 @@ public class VideoService {
     private final MemberRepository memberRepository;
     private final AmazonS3Uploader amazonS3Uploader;
 
-    public Video upload(VideoRequest videoRequest, Member member)
+    public Video uploadVideo(VideoRequest videoRequest, Member LoggedInMember)
         throws IOException {
-        Member findMember = memberRepository.findById(member.getId())
-            .orElseThrow(NotExistMemberException::new);
-
+        Member member = findMember(LoggedInMember);
+        
         S3UploadDto videoDto = amazonS3Uploader.saveToS3(videoRequest.getVideo(), "video");
         S3UploadDto thumbnailDto = amazonS3Uploader.saveToS3(videoRequest.getThumbnail(), "video/thumbnail");
 
-        Video video = videoRequest.toVideo(videoDto.getFileUrl(), thumbnailDto.getFileUrl(), findMember);
+        Video video = videoRequest.toVideo(videoDto.getFileUrl(), thumbnailDto.getFileUrl(), member);
 
         return videoRepository.save(video);
     }
+
+    public Video updateVideo(VideoUpdateRequest videoUpdateRequest, Member LoggedInMember, Long videoId) {
+        Video video = findVideo(videoId);
+        Member member = findMember(LoggedInMember);
+
+        validateAuthor(member, video);
+
+        video.update(videoUpdateRequest.toVideo());
+        return video;
+    }
+
+    private void validateAuthor(Member member, Video video) {
+        if (!video.isAuthorOf(member)) {
+            throw new NotAuthorException();
+        }
+    }
+
+    private Video findVideo(Long videoId) {
+        return videoRepository.findById(videoId).orElseThrow(NotExistVideoException::new);
+    }
+
+    private Member findMember(Member member) {
+        return memberRepository.findById(member.getId())
+            .orElseThrow(NotExistMemberException::new);
+    }
+
 }
