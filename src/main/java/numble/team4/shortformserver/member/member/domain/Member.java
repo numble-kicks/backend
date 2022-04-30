4b2e114b63cdf75edbd4d0e69425d715926e4b6a
jaeesu <wogmlsu@gmail.com>
2022-04-30 18:00:51 +0900

    refactor: Member 비교 역할을 Member 도메인에서 하도록 수정
    외부에서의 getter 호출을 줄이고, 클래스 내부에서 요소를 처리하기 위함

--- src/main/java/numble/team4/shortformserver/member/member/domain/Member.java
+++ src/main/java/numble/team4/shortformserver/member/member/domain/Member.java
@@ -3,6 +3,7 @@ package numble.team4.shortformserver.member.member.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import numble.team4.shortformserver.common.domain.BaseTimeEntity;
+import numble.team4.shortformserver.follow.exception.NotSelfFollowableException;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -25,4 +26,8 @@ public class Member extends BaseTimeEntity {
     private LocalDateTime lastLoginDate;
     private String profileImageUrl;
     private boolean emailVerified;
+
+    public boolean isEqualMember(Member member) {
+        return id.equals(member.id);
+    }
 }
