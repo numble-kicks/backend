83b903cfa381b782ce9c7f066aa8887951b7b968
chkim <cndghks0213@gmail.com>
2022-05-01 21:31:55 +0900

    feat: userId로부터 UserDetails를 생성하는 CustomUserDetaliService 구현

---
+++ src/main/java/numble/team4/shortformserver/member/auth/application/CustomUserDetailService.java
@@ -0,0 +1,38 @@
+package numble.team4.shortformserver.member.auth.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import numble.team4.shortformserver.member.auth.exception.EmailEmptyException;
+import numble.team4.shortformserver.member.member.domain.Member;
+import numble.team4.shortformserver.member.member.domain.MemberRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private static final String USER_NAME_NOT_FOUND = "사용자를 찾을 수 없습니다.";
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findById(Long.valueOf(username))
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NAME_NOT_FOUND));
+        if (member.hasNotEmail())
+            throw new EmailEmptyException();
+        return createUserDetails(member);
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        SimpleGrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRole().getDescription());
+        return new User(member.getEmail(), "", Collections.singleton(grantedAuthority));
+    }
+}
---
+++ src/test/java/numble/team4/shortformserver/member/auth/application/CustomUserDetailServiceTest.java
@@ -0,0 +1,50 @@
+package numble.team4.shortformserver.member.auth.application;
+
+import numble.team4.shortformserver.member.auth.exception.EmailEmptyException;
+import numble.team4.shortformserver.member.member.domain.Member;
+import numble.team4.shortformserver.member.member.domain.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static numble.team4.shortformserver.member.member.domain.Role.ROLE_MEMBER;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CustomUserDetailServiceTest {
+
+    @InjectMocks
+    private CustomUserDetailService userDetailService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @DisplayName("유저 이메일로 해당 유저가 존재하는지 조회한 후 있으면 기존 유저를, 없으면 새로운 유저를 등록해서 반환")
+    @Test
+    void loadUserByUsernameTest() {
+        Member member = new Member("numble@numble.com", "numble", ROLE_MEMBER, true);
+        ReflectionTestUtils.setField(member, "id", 1L);
+        given(memberRepository.findById(1L)).willReturn(Optional.of(member));
+
+        UserDetails userDetails = userDetailService.loadUserByUsername(String.valueOf(1L));
+        Assertions.assertThat(userDetails.getUsername()).isEqualTo("numble@numble.com");
+    }
+
+    @DisplayName("유저 이메일이 없으면 예외 발생")
+    @Test
+    void loadUserByUsernameTestFail() {
+        Member member = new Member(null, "numble", ROLE_MEMBER, false);
+        ReflectionTestUtils.setField(member, "id", 1L);
+        given(memberRepository.findById(1L)).willReturn(Optional.of(member));
+        Assertions.assertThatThrownBy(() -> userDetailService.loadUserByUsername(String.valueOf(1L)))
+                .isInstanceOf(EmailEmptyException.class);
+    }
+}
