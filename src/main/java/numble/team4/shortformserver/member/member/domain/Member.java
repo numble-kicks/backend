4ea79df16f52fb76fc095c51c948f72843642a2e
chkim <cndghks0213@gmail.com>
2022-05-01 21:34:36 +0900

    feat: 회원 role 추가 및 equals, hashcode 재정의

--- src/main/java/numble/team4/shortformserver/member/member/domain/Member.java
+++ src/main/java/numble/team4/shortformserver/member/member/domain/Member.java
@@ -1,5 +1,6 @@
 package numble.team4.shortformserver.member.member.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import numble.team4.shortformserver.common.domain.BaseTimeEntity;
@@ -9,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -18,11 +20,37 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 public class Member extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = IDENTITY)
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String email;
     private String name;
+    private Role role;
     private LocalDateTime lastLoginDate;
     private String profileImageUrl;
     private boolean emailVerified;
+
+    public Member(String email, String name, Role role, boolean emailVerified) {
+        this.email = email;
+        this.name = name;
+        this.role = role;
+        this.emailVerified = emailVerified;
+    }
+
+    public boolean hasNotEmail() {
+        return !emailVerified;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(email, member.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
 }
