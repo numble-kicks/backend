24c52c3980330f30f746f3004004ec467d3e8018
seongho-joo <hazzyseong@gmail.com>
2022-05-02 23:29:02 +0900

    feat: 사용자 팔로우 기능 구현 (#16)

---
+++ src/main/java/numble/team4/shortformserver/follow/domain/Follow.java
@@ -0,0 +1,38 @@
+package numble.team4.shortformserver.follow.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import numble.team4.shortformserver.follow.exception.NotSelfFollowableException;
+import numble.team4.shortformserver.member.member.domain.Member;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "from_member", nullable = false)
+    private Member fromMember;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "to_member", nullable = false)
+    private Member toMember;
+
+    public static Follow fromMembers(Member fromMember, Member toMember) {
+        if (fromMember.isEqualMember(toMember)) {
+            throw new NotSelfFollowableException();
+        }
+        return new Follow(null, fromMember, toMember);
+    }
+}
