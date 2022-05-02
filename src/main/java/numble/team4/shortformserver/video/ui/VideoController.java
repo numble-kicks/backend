73a9e3e6e875423433b54e17a98b30b0f3acf84e
seongho-joo <hazzyseong@gmail.com>
2022-05-02 16:35:20 +0900

    feat: 영상 수정 api 구현 (#14)

--- src/main/java/numble/team4/shortformserver/video/ui/VideoController.java
+++ src/main/java/numble/team4/shortformserver/video/ui/VideoController.java
@@ -1,17 +1,23 @@
 package numble.team4.shortformserver.video.ui;
 
+import static numble.team4.shortformserver.video.ui.VideoResponseMessage.UPDATE_VIDEO;
 import static numble.team4.shortformserver.video.ui.VideoResponseMessage.UPLOAD_VIDEO;
 
 import java.io.IOException;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.common.dto.CommonResponse;
 import numble.team4.shortformserver.member.member.domain.Member;
-import numble.team4.shortformserver.member.member.domain.MemberRepository;
 import numble.team4.shortformserver.video.application.VideoService;
 import numble.team4.shortformserver.video.dto.VideoRequest;
+import numble.team4.shortformserver.video.dto.VideoResponse;
+import numble.team4.shortformserver.video.dto.VideoUpdateRequest;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +28,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class VideoController {
 
     private final VideoService videoService;
-    private final MemberRepository memberRepository;
 
     @PostMapping
-    public CommonResponse saveVideo(@ModelAttribute VideoRequest videoRequest,
-        Member member) throws IOException {
-        videoService.uploadVideo(videoRequest, member);
-        return CommonResponse.from(UPLOAD_VIDEO.getMessage());
+    public CommonResponse<VideoResponse> saveVideo(@ModelAttribute VideoRequest videoRequest,
+        Member loggedInMember) throws IOException {
+        VideoResponse videoResponse = videoService.uploadVideo(videoRequest, loggedInMember);
+        return CommonResponse.of(videoResponse, UPLOAD_VIDEO.getMessage());
+    }
+
+    @PutMapping("/{videoId}")
+    public CommonResponse<VideoResponse> updateVideo(
+        @RequestBody @Valid VideoUpdateRequest videoUpdateRequest,
+        Member loggedInMember,
+        @PathVariable Long videoId) {
+        VideoResponse videoResponse = videoService.updateVideo(videoUpdateRequest, loggedInMember,
+            videoId);
+        return CommonResponse.of(videoResponse, UPDATE_VIDEO.getMessage());
     }
 }
