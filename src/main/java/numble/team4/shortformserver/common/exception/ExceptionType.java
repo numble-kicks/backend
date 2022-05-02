257246e2f79523d5970f2edbb71da8ec1a389141
seongho-joo <hazzyseong@gmail.com>
2022-05-02 23:48:52 +0900

    fix: typo 수정

--- src/main/java/numble/team4/shortformserver/common/exception/ExceptionType.java
+++ src/main/java/numble/team4/shortformserver/common/exception/ExceptionType.java
@@ -1,17 +1,20 @@
 package numble.team4.shortformserver.common.exception;
 
+import static org.springframework.http.HttpStatus.*;
+
+import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.follow.exception.AlreadyExistFollowException;
 import numble.team4.shortformserver.follow.exception.NotSelfFollowableException;
+import numble.team4.shortformserver.aws.exception.AmazonClientException;
+import numble.team4.shortformserver.aws.exception.NotExistFileException;
 import numble.team4.shortformserver.member.auth.exception.WrongPasswordException;
+import numble.team4.shortformserver.member.member.exception.NotAuthorException;
 import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
 import numble.team4.shortformserver.video.exception.NotExistVideoException;
+import numble.team4.shortformserver.member.auth.exception.NotLoggedInException;
 import org.springframework.http.HttpStatus;
-
-import java.util.Arrays;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Getter
 @RequiredArgsConstructor
@@ -20,7 +23,12 @@ public enum ExceptionType {
     NOT_EXIST_VIDEO("존재하지 않는 영상입니다.", BAD_REQUEST, NotExistVideoException.class),
     WRONG_PASSWORD("비밀번호가 틀렸습니다.", BAD_REQUEST, WrongPasswordException.class),
     ALREADY_EXIST_FOLLOW("이미 팔로우가 되어있는 사용자입니다.", BAD_REQUEST, AlreadyExistFollowException.class),
-    NOT_SELF_FOLLOW_ABLE("본인은 팔로우 할 수 없습니다.", BAD_REQUEST, NotSelfFollowableException.class);
+    NOT_SELF_FOLLOW_ABLE("본인은 팔로우 할 수 없습니다.", BAD_REQUEST, NotSelfFollowableException.class),
+    NOT_EXIST_FILE("존재하지 않는 파일입니다.", BAD_REQUEST, NotExistFileException.class),
+    AMAZON_CLIENT_EXCEPTION("아마존 서버에 업로드하는 과정에서 오류가 발생하였습니다.", INTERNAL_SERVER_ERROR, AmazonClientException.class),
+    NOT_LOGGED_IN("로그인이 되어있지 않습니다.", UNAUTHORIZED, NotLoggedInException.class),
+    NOT_AUTHOR_EXCEPTION("작성자가 아니므로 권한이 없습니다.",UNAUTHORIZED, NotAuthorException.class);
+
 
     private final String message;
     private final HttpStatus status;
