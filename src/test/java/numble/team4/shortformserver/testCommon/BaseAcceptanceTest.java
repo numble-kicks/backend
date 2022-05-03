d727ec8bec5b9870f79227077e8a351f5d1812d8
jaeesu <wogmlsu@gmail.com>
2022-05-03 18:31:40 +0900

    test: 테스트 환경 지정 애노테이션 추가

--- src/test/java/numble/team4/shortformserver/testCommon/BaseAcceptanceTest.java
+++ src/test/java/numble/team4/shortformserver/testCommon/BaseAcceptanceTest.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
 @Disabled
 @Transactional
+@ActiveProfiles("local")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BaseAcceptanceTest {
