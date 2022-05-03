303e56c28d0e107f7cf74eb8a403b5db3272d9ac
jaeesu <wogmlsu@gmail.com>
2022-05-03 18:08:47 +0900

    test: 인수테스트 설정 상속을 위한 클래스 추가

---
+++ src/test/java/numble/team4/shortformserver/testCommon/BaseAcceptanceTest.java
@@ -0,0 +1,27 @@
+package numble.team4.shortformserver.testCommon;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+@Disabled
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseAcceptanceTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @LocalServerPort
+    protected int port;
+}
