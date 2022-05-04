32be501d18ff2ffa281d101f1a6a51de419a19d5
chkim <cndghks0213@gmail.com>
2022-05-04 20:52:42 +0900

    refactor: SecurityContextHoler에서 MemberAdapter를 가져와 member를 반환하도록 변경

--- src/main/java/numble/team4/shortformserver/member/auth/util/LoginUserInfoArgumentResolver.java
+++ src/main/java/numble/team4/shortformserver/member/auth/util/LoginUserInfoArgumentResolver.java
@@ -2,9 +2,12 @@ package numble.team4.shortformserver.member.auth.util;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import numble.team4.shortformserver.member.auth.domain.MemberAdapter;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.MemberRepository;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -16,22 +19,20 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class JwtTokenArgumentResolver implements HandlerMethodArgumentResolver {
-    private final MemberRepository memberRepository;
-    private final JwtTokenProvider jwtTokenProvider;
+public class LoginUserInfoArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         boolean hasMemberInfoAnnotation = parameter.hasParameterAnnotation(LoginUser.class);
-        boolean hasMemberInfoDtoType = Member.class.isAssignableFrom(parameter.getParameterType());
-        return hasMemberInfoAnnotation && hasMemberInfoDtoType;
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        return hasMemberInfoAnnotation && hasMemberType;
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String token = jwtTokenProvider.extractToken(request);
-        return memberRepository.findById(jwtTokenProvider.getUserIdFromAccessToken(token))
-                .orElseThrow(IllegalArgumentException::new);
+        MemberAdapter memberAdapter = (MemberAdapter) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        return memberAdapter.getMember();
     }
 }
