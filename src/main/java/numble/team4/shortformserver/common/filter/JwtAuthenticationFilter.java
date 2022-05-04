1b20494c38e163c28ab6e0c5f66c58edad48df32
chkim <cndghks0213@gmail.com>
2022-05-04 20:43:58 +0900

    refactor: GenericFilterBean -> OncePerRequestFilter로 변경

--- src/main/java/numble/team4/shortformserver/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/numble/team4/shortformserver/common/filter/JwtAuthenticationFilter.java
@@ -1,30 +1,29 @@
 package numble.team4.shortformserver.common.filter;
 
 import lombok.RequiredArgsConstructor;
-import numble.team4.shortformserver.member.auth.exception.JwtTokenExpiredException;
 import numble.team4.shortformserver.member.auth.util.JwtTokenProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String token = jwtTokenProvider.extractToken((HttpServletRequest) request);
-        if (token != null && jwtTokenProvider.validateTokenSecretToken(token)) {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.extractToken(request);
+        if (!StringUtils.hasText(token) && jwtTokenProvider.validateTokenSecretToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/numble/team4/shortformserver/common/filter/JwtExceptionHandleFilter.java
+++ src/main/java/numble/team4/shortformserver/common/filter/JwtExceptionHandleFilter.java
@@ -6,29 +6,31 @@ import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.common.exception.BaseErrorResponse;
 import numble.team4.shortformserver.common.exception.BaseException;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.fasterxml.jackson.core.json.JsonWriteFeature.ESCAPE_NON_ASCII;
 
 @RequiredArgsConstructor
-public class JwtExceptionHandleFilter extends GenericFilterBean {
+public class JwtExceptionHandleFilter extends OncePerRequestFilter {
 
     private static final String APPLICATION_JSON = "application/json; charset:UTF-8";
     private final ObjectMapper objectMapper;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
         try {
-            chain.doFilter(request, response);
+            filterChain.doFilter(request, response);
         } catch (Exception exception) {
             assert exception instanceof BaseException;
-            setErrorResponse((HttpServletResponse) response, (BaseException) exception);
+            setErrorResponse(response, (BaseException) exception);
         }
     }
 
