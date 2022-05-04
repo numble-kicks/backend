6644dbeb17ddaf1f35b2a8eef95f8e9ef0167b79
chkim <cndghks0213@gmail.com>
2022-05-04 20:44:29 +0900

    refactor: 중복되는 메서드를 OauthProvider의 static method로 분리

--- src/main/java/numble/team4/shortformserver/member/auth/application/membercreator/MemberCreatorFactory.java
+++ src/main/java/numble/team4/shortformserver/member/auth/application/membercreator/MemberCreatorFactory.java
@@ -1,14 +1,14 @@
 package numble.team4.shortformserver.member.auth.application.membercreator;
 
 import numble.team4.shortformserver.member.auth.domain.OauthProvider;
-import numble.team4.shortformserver.member.auth.exception.NotExistProviderException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
+import static numble.team4.shortformserver.member.auth.domain.OauthProvider.findMatchProvider;
 
 @Component
 public class MemberCreatorFactory {
@@ -22,13 +22,6 @@ public class MemberCreatorFactory {
 
     public MemberCreatorFromSocialInfo findMemberCreator(String providerName) {
         return providers.get(findMatchProvider(providerName));
-    }
-
-    private OauthProvider findMatchProvider(String providerName) {
-        return Arrays.stream(OauthProvider.values())
-                .filter(provider -> provider.isEqualProviderName(providerName))
-                .findAny()
-                .orElseThrow(NotExistProviderException::new);
     }
 
     private void registerCreators(Set<MemberCreatorFromSocialInfo> creators) {
--- src/main/java/numble/team4/shortformserver/member/auth/domain/OauthProvider.java
+++ src/main/java/numble/team4/shortformserver/member/auth/domain/OauthProvider.java
@@ -2,6 +2,9 @@ package numble.team4.shortformserver.member.auth.domain;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import numble.team4.shortformserver.member.auth.exception.NotExistProviderException;
+
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,5 +15,12 @@ public enum OauthProvider {
 
     public boolean isEqualProviderName(String providerName) {
         return this.providerName.equals(providerName);
+    }
+
+    public static OauthProvider findMatchProvider(String providerName) {
+        return Arrays.stream(OauthProvider.values())
+                .takeWhile(provider -> provider.isEqualProviderName(providerName))
+                .findAny()
+                .orElseThrow(NotExistProviderException::new);
     }
 }
