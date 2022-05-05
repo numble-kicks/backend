2eee01fffd91449764d6da303dd702750909b9df
seongho-joo <hazzyseong@gmail.com>
2022-05-05 16:33:44 +0900

    refactor: 파라미터 수정 및 검증 메서드 제거

--- src/main/java/numble/team4/shortformserver/video/application/VideoService.java
+++ src/main/java/numble/team4/shortformserver/video/application/VideoService.java
@@ -1,14 +1,10 @@
 package numble.team4.shortformserver.video.application;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.aws.application.AmazonS3Uploader;
 import numble.team4.shortformserver.aws.dto.S3UploadDto;
 import numble.team4.shortformserver.member.member.domain.Member;
-import numble.team4.shortformserver.member.member.domain.MemberRepository;
-import numble.team4.shortformserver.member.member.exception.NotAuthorException;
-import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
 import numble.team4.shortformserver.video.domain.Video;
 import numble.team4.shortformserver.video.domain.VideoRepository;
 import numble.team4.shortformserver.video.dto.VideoRequest;
@@ -25,48 +21,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class VideoService {
 
     private final VideoRepository videoRepository;
-    private final MemberRepository memberRepository;
     private final AmazonS3Uploader amazonS3Uploader;
 
     @Transactional
-    public VideoResponse uploadVideo(VideoRequest videoRequest, Long memberId)
-        throws IOException {
-        Member member = findMember(memberId);
-        
+    public VideoResponse uploadVideo(VideoRequest videoRequest, Member loggedInMember) {
         S3UploadDto videoDto = amazonS3Uploader.saveToS3(videoRequest.getVideo(), "video");
         S3UploadDto thumbnailDto = amazonS3Uploader.saveToS3(videoRequest.getThumbnail(), "video/thumbnail");
 
-        Video video = videoRequest.toVideo(videoDto.getFileUrl(), thumbnailDto.getFileUrl(), member);
+        Video video = videoRequest.toVideo(videoDto.getFileUrl(), thumbnailDto.getFileUrl(), loggedInMember);
 
         Video saveVideo = videoRepository.save(video);
-        saveVideo.addVideoToMember(member);
-        return VideoResponse.of(saveVideo);
+        return VideoResponse.from(saveVideo);
     }
 
     @Transactional
-    public VideoResponse updateVideo(VideoUpdateRequest videoUpdateRequest, Long memberId, Long videoId) {
+    public VideoResponse updateVideo(VideoUpdateRequest videoUpdateRequest, Member loggedInMember, Long videoId) {
         Video video = findVideo(videoId);
-        Member member = findMember(memberId);
 
-        validateAuthor(member, video);
-
-        video.update(videoUpdateRequest.toVideo());
-        return VideoResponse.of(video);
+        video.update(videoUpdateRequest.getTitle(), videoUpdateRequest.getDescription(), loggedInMember);
+        return VideoResponse.from(video);
     }
 
-    private void validateAuthor(Member member, Video video) {
-        if (!video.isAuthorOf(member)) {
-            throw new NotAuthorException();
-        }
-    }
 
     private Video findVideo(Long videoId) {
         return videoRepository.findById(videoId).orElseThrow(NotExistVideoException::new);
     }
-
-    private Member findMember(Long memberId) {
-        return memberRepository.findById(memberId)
-            .orElseThrow(NotExistMemberException::new);
-    }
-
 }
