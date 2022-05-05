e024700a64af78b230dca441fa830f19d49d4fe8
jaeesu <wogmlsu@gmail.com>
2022-05-05 18:00:48 +0900

    test: 로컬 테스트 환경에서 local db를 사용하기 위한 설정 추가

--- src/test/java/numble/team4/shortformserver/ShortFormServerApplicationTests.java
+++ src/test/java/numble/team4/shortformserver/ShortFormServerApplicationTests.java
@@ -2,8 +2,10 @@ package numble.team4.shortformserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class ShortFormServerApplicationTests {
 
 	@Test
