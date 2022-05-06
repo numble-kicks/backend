a23ec60020125831d62626d2c98a3385115a6ded
jaeesu <wogmlsu@gmail.com>
2022-05-07 01:23:16 +0900

    refactor: 커스텀 레포지토리와 중복되는 기능 삭제

--- src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
+++ src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
@@ -3,14 +3,8 @@ package numble.team4.shortformserver.follow.domain;
 import numble.team4.shortformserver.follow.infrastructure.FollowCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FollowRepository extends JpaRepository<Follow, Long>, FollowCustomRepository {
 
     boolean existsByFromMember_IdAndToMember_Id(Long fromMember, Long toMember);
-
-    List<Follow> findByFromMember_Id(Long id);
-
-    List<Follow> findByToMember_Id(Long id);
 
 }
