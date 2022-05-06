67e211287ab741d49f7e7d838a812de65701297b
jaeesu <wogmlsu@gmail.com>
2022-05-06 22:12:24 +0900

    feat: follow 레포지토리 팔로우 목록 조회 메서드 테스트 추가

--- src/test/java/numble/team4/shortformserver/follow/domain/FollowRepositoryTest.java
+++ src/test/java/numble/team4/shortformserver/follow/domain/FollowRepositoryTest.java
@@ -2,13 +2,19 @@ package numble.team4.shortformserver.follow.domain;
 
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.MemberRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.*;
+import numble.team4.shortformserver.member.member.domain.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @ActiveProfiles("local")
@@ -28,9 +34,11 @@ class FollowRepositoryTest {
     @BeforeEach
     void init() {
         fromUser = Member.builder()
+                .role(Role.ROLE_MEMBER)
                 .emailVerified(true)
                 .build();
         toUser = Member.builder()
+                .role(Role.ROLE_MEMBER)
                 .emailVerified(true)
                 .build();
         memberRepository.save(fromUser);
@@ -39,7 +47,7 @@ class FollowRepositoryTest {
 
     @Nested
     @DisplayName("팔로우 저장 테스트")
-    class saveFollowTest {
+    class SaveFollowTest {
 
         @Test
         @DisplayName("[성공] 정상적인 상태의 팔로우")
@@ -51,8 +59,43 @@ class FollowRepositoryTest {
             followRepository.save(follow);
 
             //then
-            Assertions.assertThat(followRepository.findAll()).hasSize(1);
+            assertThat(followRepository.findAll()).hasSize(1);
         }
+
+    }
+
+    @Nested
+    @DisplayName("팔로우 조회 테스트")
+    class GetFollowTest {
+
+        @Test
+        @DisplayName("[성공] 팔로잉 조회 테스트")
+        void findByFromMember_Id_getFollowIsEqualFromMember_success() {
+            //given
+            Follow follow = Follow.fromMembers(fromUser, toUser);
+            followRepository.save(follow);
+
+            //when
+            List<Follow> followings = followRepository.findByFromMember_Id(fromUser.getId());
+
+            //then
+            assertThat(followings).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("[성공] 팔로워 조회 테스트")
+        void findByFromMember_Id_getFollowIsEqualToMember_success() {
+            //given
+            Follow follow = Follow.fromMembers(fromUser, toUser);
+            followRepository.save(follow);
+
+            //when
+            List<Follow> followings = followRepository.findByToMember_Id(toUser.getId());
+
+            //then
+            assertThat(followings).hasSize(1);
+        }
+
 
     }
 
