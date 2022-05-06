c7e54bb85ab36864a4a811bc4eebd00566abcfdf
jaeesu <wogmlsu@gmail.com>
2022-05-06 22:12:03 +0900

    feat: follow 레포지토리에 사용자 아이디로 팔로우, 팔로잉 목록을 가져오는 메서드 추가

--- src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
+++ src/main/java/numble/team4/shortformserver/follow/domain/FollowRepository.java
@@ -2,7 +2,14 @@ package numble.team4.shortformserver.follow.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByFromMember_IdAndToMember_Id(Long fromMember, Long toMember);
+
+    List<Follow> findByFromMember_Id(Long id);
+
+    List<Follow> findByToMember_Id(Long id);
+
 }
