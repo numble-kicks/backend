677698247db76d7a9ef368b0e25e0360ec487f8c
seongho-joo <hazzyseong@gmail.com>
2022-05-07 00:48:25 +0900

    feat: 영상 목록 조회 추가 * feat: 좋아요 순 * feat: 조회 순 * feat: 사용자의 영상 목록

--- src/main/java/numble/team4/shortformserver/video/domain/VideoRepository.java
+++ src/main/java/numble/team4/shortformserver/video/domain/VideoRepository.java
@@ -1,6 +1,20 @@
 package numble.team4.shortformserver.video.domain;
 
+import java.util.List;
+import numble.team4.shortformserver.member.member.domain.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
+
+    @Query("select v from Video v order by v.likeCount desc")
+    List<Video> findAllSortByLikeCount(Pageable pageable);
+
+    @Query("select v from Video v order by v.viewCount desc")
+    List<Video> findAllSortByViewCount(Pageable pageable);
+
+    @Query("select v from Video v where v.member = :member order by v.createAt desc")
+    List<Video> findAllByMember(@Param("member") Member member, Pageable pageable);
 }
