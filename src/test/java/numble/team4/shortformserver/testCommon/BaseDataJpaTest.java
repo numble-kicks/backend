e607cbd0449018a1e31d2f22a1c4aa321ed9abf8
jaeesu <wogmlsu@gmail.com>
2022-05-07 00:32:16 +0900

    test: jpaTest 공통 커스텀 애노테이션 생성

---
+++ src/test/java/numble/team4/shortformserver/testCommon/BaseDataJpaTest.java
@@ -0,0 +1,18 @@
+package numble.team4.shortformserver.testCommon;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public @interface BaseDataJpaTest {
+}
