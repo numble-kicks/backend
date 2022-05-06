c9332eceaa60e95b8072a08982d1fd35a8df3a58
seongho-joo <hazzyseong@gmail.com>
2022-05-07 02:35:47 +0900

    feat: cursor based pagination 추가

--- src/main/java/numble/team4/shortformserver/video/domain/VideoRepository.java
+++ src/main/java/numble/team4/shortformserver/video/domain/VideoRepository.java
@@ -12,9 +12,18 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
     @Query("select v from Video v order by v.likeCount desc")
     List<Video> findAllSortByLikeCount(Pageable pageable);
 
+    @Query("select v from Video v where v.id < :videoId order by v.likeCount desc")
+    List<Video> cursorBasedFindAllSortByLikeCount(@Param("videoId") Long videoId, Pageable pageable);
+
     @Query("select v from Video v order by v.viewCount desc")
     List<Video> findAllSortByViewCount(Pageable pageable);
 
+    @Query("select v from Video v where v.id < :videoId order by v.viewCount desc")
+    List<Video> cursorBasedFindAllSortByViewCount(@Param("videoId") Long videoId, Pageable pageable);
+
     @Query("select v from Video v where v.member = :member order by v.createAt desc")
-    List<Video> findAllByMember(@Param("member") Member member, Pageable pageable);
+    List<Video> findAllVideosOfMember(@Param("member") Member member, Pageable pageable);
+
+    @Query("select v from Video v where v.member = :member and v.id < :videoId order by v.createAt desc ")
+    List<Video> cursorBasedFindAllVideosOfMember(@Param("videoId") Long videoId, @Param("member") Member member, Pageable pageable);
 }
