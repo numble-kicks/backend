cc16ee8e8bb26831ec6955bddbe7fed744a7cb16
jaeesu <wogmlsu@gmail.com>
2022-05-07 21:10:14 +0900

    feat: 팔로우 목록 조회 api 항상 접근 허용

--- src/main/java/numble/team4/shortformserver/common/config/SecurityConfig.java
+++ src/main/java/numble/team4/shortformserver/common/config/SecurityConfig.java
@@ -6,10 +6,12 @@ import numble.team4.shortformserver.common.filter.JwtAuthenticationFilter;
 import numble.team4.shortformserver.common.filter.JwtExceptionHandleFilter;
 import numble.team4.shortformserver.member.auth.util.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -34,6 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/v1/users/following/from", "/v1/users/following/to").permitAll()
                 .antMatchers("/oauth/**").permitAll()
                 .anyRequest().authenticated();
     }
