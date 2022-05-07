41b0e07d14c715546235c4b231a48e4f6c1c2130
jaeesu <wogmlsu@gmail.com>
2022-05-07 19:52:55 +0900

    feat: 팔로우 목록 조회 api 추가

--- src/main/java/numble/team4/shortformserver/follow/ui/FollowController.java
+++ src/main/java/numble/team4/shortformserver/follow/ui/FollowController.java
@@ -3,13 +3,15 @@ package numble.team4.shortformserver.follow.ui;
 import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.common.dto.CommonResponse;
 import numble.team4.shortformserver.follow.application.FollowService;
+import numble.team4.shortformserver.follow.ui.dto.FollowResponse;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.MemberRepository;
 import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
 import org.springframework.web.bind.annotation.*;
 
-import static numble.team4.shortformserver.follow.ui.FollowResponseMessage.CREATE_FOLLOW;
-import static numble.team4.shortformserver.follow.ui.FollowResponseMessage.DELETE_FOLLOW;
+import java.util.List;
+
+import static numble.team4.shortformserver.follow.ui.FollowResponseMessage.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +20,18 @@ public class FollowController {
 
     private final MemberRepository memberRepository;
     private final FollowService followService;
+
+    @GetMapping("/from")
+    public CommonResponse getAllFollowings(@RequestParam("from_member") Long memberId) {
+        List<FollowResponse> followings = followService.getAllFollowings(memberId);
+        return CommonResponse.of(followings, GET_FOLLOWINGS.getMessage());
+    }
+
+    @GetMapping("/to")
+    public CommonResponse getAllFollowers(@RequestParam("to_member") Long memberId) {
+        List<FollowResponse> followers = followService.getAllFollowers(memberId);
+        return CommonResponse.of(followers, GET_FOLLOWERS.getMessage());
+    }
 
     @GetMapping("/{toUserId}")
     public CommonResponse createFollow(@RequestParam("from_member") Long fromMemberId, @PathVariable("toUserId") Long toMemberId) {
