a3b4f0c03f4e3c719a6d811e2477e96aa5197211
jaeesu <wogmlsu@gmail.com>
2022-05-07 21:10:40 +0900

    test: 팔로우 목록 조회 api 인수 테스트 추가

--- src/test/java/numble/team4/shortformserver/follow/acceptance/FollowingAcceptanceTest.java
+++ src/test/java/numble/team4/shortformserver/follow/acceptance/FollowingAcceptanceTest.java
@@ -16,9 +16,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.Arrays;
 
 import static numble.team4.shortformserver.common.exception.ExceptionType.*;
-import static numble.team4.shortformserver.follow.ui.FollowResponseMessage.DELETE_FOLLOW;
+import static numble.team4.shortformserver.follow.ui.FollowResponseMessage.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -92,6 +93,76 @@ public class FollowingAcceptanceTest extends BaseAcceptanceTest {
             //then
             res.andExpect(status().isBadRequest())
                     .andExpect(jsonPath("$.message").value(NOT_FOLLOWING.getMessage()));
+        }
+    }
+    
+    @Nested
+    @DisplayName("팔로우 목록 조회 테스트")
+    class GetFollowsTest {
+        
+        @Test
+        @DisplayName("[성공] 1. [팔로잉] 존재하는 사용자의 팔로잉 목로 조회 요청")
+        void getAllFollowings_isok_success() throws Exception {
+            //given
+            Follow follow = followRepository.save(Follow.fromMembers(user1, user2));
+
+            //when
+            ResultActions res = mockMvc.perform(get(baseUri + "/from")
+                    .queryParam("from_member", String.valueOf(user1.getId()))
+            );
+
+            //then
+            res.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.size()").value(1))
+                    .andExpect(jsonPath("$.message").value(GET_FOLLOWINGS.getMessage()))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("[실패] 2. [팔로잉] 존재하지 않는 사용자의 팔로잉 목록 조회 요청")
+        void getAllFollowings_notExistMemberException_fail() throws Exception {
+            //when
+            ResultActions res = mockMvc.perform(get(baseUri + "/from")
+                    .queryParam("from_member", String.valueOf(12312L))
+            );
+
+            //then
+            res.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.message").value(NOT_EXIST_MEMBER.getMessage()))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("[성공] 1. [팔로워] 존재하는 사용자의 팔로워 목로 조회 요청")
+        void getAllFollowers_isok_success() throws Exception {
+            //given
+            Follow follow = followRepository.save(Follow.fromMembers(user1, user2));
+
+            //when
+            ResultActions res = mockMvc.perform(get(baseUri + "/to")
+                    .queryParam("to_member", String.valueOf(user2.getId()))
+            );
+
+            //then
+            res.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.size()").value(1))
+                    .andExpect(jsonPath("$.message").value(GET_FOLLOWERS.getMessage()))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("[실패] 2. [팔로워] 존재하지 않는 사용자의 팔로워 목록 조회 요청")
+        void getAllFollowers_notExistMemberException_fail() throws Exception {
+            //when
+            ResultActions res = mockMvc.perform(get(baseUri + "/to")
+                    .queryParam("to_member", String.valueOf(12312))
+            );
+
+            //then
+            res.andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.message").value(NOT_EXIST_MEMBER.getMessage()))
+                    .andDo(print());
+
         }
     }
 }
