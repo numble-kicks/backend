3ebfc4defb2ef04efbbcaad1d6fc5568cba1b3ce
jaeesu <wogmlsu@gmail.com>
2022-05-09 01:01:12 +0900

    feat: LikeVideo 레포지토리 추가

---
+++ src/main/java/numble/team4/shortformserver/likevideo/domain/LikeVideoRepository.java
@@ -0,0 +1,6 @@
+package numble.team4.shortformserver.likevideo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeVideoRepository extends JpaRepository<LikeVideo, Long> {
+}
