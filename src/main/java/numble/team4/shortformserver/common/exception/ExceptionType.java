51c4b6b393aa65e3f58b4f23a57b400ceaf171d4
jaeesu <wogmlsu@gmail.com>
2022-05-09 02:13:57 +0900

    feat: 좋아요를 누른 동영상에 또 좋아요를 누를 경우 발생하는 예외 추가

--- src/main/java/numble/team4/shortformserver/common/exception/ExceptionType.java
+++ src/main/java/numble/team4/shortformserver/common/exception/ExceptionType.java
@@ -1,20 +1,15 @@
 package numble.team4.shortformserver.common.exception;
 
-import static org.springframework.http.HttpStatus.*;
-
-import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import numble.team4.shortformserver.member.auth.exception.*;
+import numble.team4.shortformserver.aws.exception.AmazonClientException;
+import numble.team4.shortformserver.aws.exception.NotExistFileException;
 import numble.team4.shortformserver.follow.exception.AlreadyExistFollowException;
 import numble.team4.shortformserver.follow.exception.NotExistFollowException;
 import numble.team4.shortformserver.follow.exception.NotFollowingException;
 import numble.team4.shortformserver.follow.exception.NotSelfFollowableException;
-import numble.team4.shortformserver.aws.exception.AmazonClientException;
-import numble.team4.shortformserver.aws.exception.NotExistFileException;
-import numble.team4.shortformserver.member.auth.exception.WrongPasswordException;
-
+import numble.team4.shortformserver.likevideo.exception.AlreadyExistLikeVideoException;
+import numble.team4.shortformserver.member.auth.exception.*;
 import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
 import numble.team4.shortformserver.video.exception.NotExistVideoException;
 import numble.team4.shortformserver.video.exception.NotLoggedInException;
@@ -41,7 +36,8 @@ public enum ExceptionType {
     AMAZON_CLIENT_EXCEPTION("아마존 서버에 업로드하는 과정에서 오류가 발생하였습니다.", INTERNAL_SERVER_ERROR, AmazonClientException.class),
     NOT_LOGGED_IN("로그인이 되어있지 않습니다.", UNAUTHORIZED, NotLoggedInException.class),
     NOT_EXIST_FOLLOW("존재하지 않는 팔로우입니다.", BAD_REQUEST, NotExistFollowException.class),
-    NOT_FOLLOWING("본인의 팔로우만 삭제할 수 있습니다.", BAD_REQUEST, NotFollowingException.class);
+    NOT_FOLLOWING("본인의 팔로우만 삭제할 수 있습니다.", BAD_REQUEST, NotFollowingException.class),
+    ALREADY_EXIST_LIKE_VIDEO("이미 좋아요를 등록한 동영상입니다.", BAD_REQUEST, AlreadyExistLikeVideoException.class);
 
 
     private final String message;
