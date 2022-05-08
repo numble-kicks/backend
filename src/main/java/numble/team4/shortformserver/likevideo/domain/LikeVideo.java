b702dd833e887cca929ab5ba7a2269ed29831d25
jaeesu <wogmlsu@gmail.com>
2022-05-09 01:12:53 +0900

    feat: likevideo 생성자 대신 사용할 정적 팩터리 메서드 생성

--- src/main/java/numble/team4/shortformserver/likevideo/domain/LikeVideo.java
+++ src/main/java/numble/team4/shortformserver/likevideo/domain/LikeVideo.java
@@ -1,5 +1,6 @@
 package numble.team4.shortformserver.likevideo.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.video.domain.Video;
@@ -11,6 +12,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@AllArgsConstructor(access = PROTECTED)
 @NoArgsConstructor(access = PROTECTED)
 public class LikeVideo {
 
@@ -24,4 +26,8 @@ public class LikeVideo {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "video_id")
     private Video video;
+
+    public static LikeVideo fromMemberAndVideo(Member member, Video video) {
+        return new LikeVideo(null, member, video);
+    }
 }
