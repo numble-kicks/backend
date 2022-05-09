a817c885c366e66648d340e41e79b1faf023c94b
jaeesu <wogmlsu@gmail.com>
2022-05-10 02:03:30 +0900

    refactor: 동영상 좋아요 등록 여부 api 반환값 프로퍼티 수정

--- src/main/java/numble/team4/shortformserver/likevideo/ui/dto/LikeVideoExistsResponse.java
+++ src/main/java/numble/team4/shortformserver/likevideo/ui/dto/LikeVideoExistsResponse.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class LikeVideoExistsResponse {
 
-    private boolean likeVideoExists;
+    private boolean isExistLikeVideo;
 
     public static LikeVideoExistsResponse from(boolean likeVideoExists) {
         return new LikeVideoExistsResponse(likeVideoExists);
