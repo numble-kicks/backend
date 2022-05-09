1abc0ae87987d356965ed844af70c7bba4b3ac30
jaeesu <wogmlsu@gmail.com>
2022-05-10 00:15:12 +0900

    test: member, video 변수를 전역변수로 수정

--- src/test/java/numble/team4/shortformserver/likevideo/domain/LikeVideoRepositoryTest.java
+++ src/test/java/numble/team4/shortformserver/likevideo/domain/LikeVideoRepositoryTest.java
@@ -4,6 +4,7 @@ import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.Role;
 import numble.team4.shortformserver.testCommon.BaseDataJpaTest;
 import numble.team4.shortformserver.video.domain.Video;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,26 +23,30 @@ class LikeVideoRepositoryTest {
     @Autowired
     private LikeVideoRepository likeVideoRepository;
 
-    @Test
-    @DisplayName("[성공] likeVideo 추가 테스트")
-    void save_LikeVideoListSizeOne_success() {
-        //given
-        Member member = Member.builder()
+    private Member member;
+    private Video video;
+
+    @BeforeEach
+    void init() {
+        member = Member.builder()
                 .role(Role.MEMBER)
                 .emailVerified(true)
                 .build();
         entityManager.persist(member);
 
-        Video video = Video.builder()
+        video = Video.builder()
                 .videoUrl("http://videourl.com")
                 .thumbnailUrl("http://url.com")
                 .title("title")
                 .description("description")
                 .build();
         entityManager.persist(video);
+    }
 
+    @Test
+    @DisplayName("[성공] likeVideo 추가 테스트")
+    void save_LikeVideoListSizeOne_success() {
         //when
-
         LikeVideo likeVideo = LikeVideo.fromMemberAndVideo(member, video);
         likeVideoRepository.save(likeVideo);
 
@@ -52,4 +57,10 @@ class LikeVideoRepositoryTest {
 
     }
 
+
+    @Test
+    @DisplayName("[성공] likeVideo 삭제 테스트")
+    void delete_LikeVideoListSizeZero_success() {
+
+    }
 }
