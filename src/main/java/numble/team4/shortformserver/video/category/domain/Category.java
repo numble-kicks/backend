b6e1d97ca7b3e4cafcb4b6b5a8ad4afde4d5a17b
seongho-joo <hazzyseong@gmail.com>
2022-05-10 23:42:10 +0900

    feat: Category 엔티티 및 리포지토리 생성

---
+++ src/main/java/numble/team4/shortformserver/video/category/domain/Category.java
@@ -0,0 +1,26 @@
+package numble.team4.shortformserver.video.category.domain;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import numble.team4.shortformserver.common.domain.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
---
+++ src/main/java/numble/team4/shortformserver/video/category/domain/CategoryRepository.java
@@ -0,0 +1,7 @@
+package numble.team4.shortformserver.video.category.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
