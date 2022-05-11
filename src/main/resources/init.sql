8cc9a6ff8ae38cf0e981d8e8bb5339c896a0e758
seongho-joo <hazzyseong@gmail.com>
2022-05-11 17:17:36 +0900

    feat: 카테고리 테이블 추가 * 비디오 테이블 카테고리 FK 추가 * 카테고리 INSERT SQL 추가

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,62 +2,94 @@ DROP TABLE IF EXISTS like_video;
 DROP TABLE IF EXISTS video;
 DROP TABLE IF EXISTS follow;
 DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS category;
 
+CREATE TABLE category
+(
+    id          bigint       not null auto_increment,
+    create_at   timestamp default current_timestamp,
+    modified_at timestamp default current_timestamp on update current_timestamp,
+    name        varchar(255) not null,
+    primary key (id)
+) ENGINE = InnoDB;
 
 CREATE TABLE video
 (
     id            bigint       not null auto_increment,
+    create_at     datetime(6),
+    modified_at   datetime(6),
     description   varchar(255),
-    like_count    bigint DEFAULT 0,
+    like_count    bigint,
+    price         integer      not null,
     thumbnail_url varchar(255) not null,
     title         varchar(255) not null,
+    used_status   bit          not null,
     video_url     varchar(255) not null,
-    view_count    bigint DEFAULT 0,
+    view_count    bigint,
+    category_id   bigint,
     member_id     bigint,
-    create_at   datetime(6) DEFAULT NULL,
-    modified_at datetime(6) DEFAULT NULL,
     primary key (id)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE like_video
 (
-    id bigint NOT NULL AUTO_INCREMENT,
+    id        bigint NOT NULL AUTO_INCREMENT,
     member_id bigint NOT NULL,
-    video_id bigint NOT NULL,
+    video_id  bigint NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE member
 (
-    id bigint NOT NULL AUTO_INCREMENT,
-    create_at datetime(6) DEFAULT NULL,
-    modified_at datetime(6) DEFAULT NULL,
+    id                bigint      NOT NULL AUTO_INCREMENT,
+    create_at         datetime(6) DEFAULT NULL,
+    modified_at       datetime(6) DEFAULT NULL,
     email             varchar(255),
-    email_verified    bit NOT NULL,
+    email_verified    bit         NOT NULL,
     last_login_date   datetime(6),
     name              varchar(255),
     profile_image_url varchar(255),
     role              varchar(30) NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE follow
 (
-    id bigint NOT NULL AUTO_INCREMENT,
+    id          bigint NOT NULL AUTO_INCREMENT,
     from_member bigint NOT NULL,
-    to_member bigint NOT NULL,
+    to_member   bigint NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
 
-ALTER TABLE like_video ADD CONSTRAINT FOREIGN KEY (member_id) REFERENCES member (id);
+ALTER TABLE like_video
+    ADD CONSTRAINT FOREIGN KEY (member_id) REFERENCES member (id);
 
+ALTER TABLE like_video
+    ADD CONSTRAINT FOREIGN KEY (video_id) REFERENCES video (id);
 
-ALTER TABLE like_video ADD CONSTRAINT FOREIGN KEY (video_id) REFERENCES video (id);
+ALTER TABLE follow
+    ADD CONSTRAINT FOREIGN KEY (from_member) REFERENCES member (id);
 
-ALTER TABLE follow ADD CONSTRAINT FOREIGN KEY (from_member) REFERENCES member (id);
-
-ALTER TABLE follow ADD CONSTRAINT FOREIGN KEY (to_member) REFERENCES member (id);
+ALTER TABLE follow
+    ADD CONSTRAINT FOREIGN KEY (to_member) REFERENCES member (id);
 
 ALTER TABLE video
-    ADD CONSTRAINT FOREIGN KEY (member_id) REFERENCES member (id)
+    ADD CONSTRAINT FOREIGN KEY (member_id) REFERENCES member (id);
+
+ALTER TABLE video
+    ADD CONSTRAINT FOREIGN KEY (category_id) REFERENCES category (id);
+
+ALTER TABLE category
+    ADD CONSTRAINT UNIQUE (name);
+
+INSERT INTO category (name)
+    VALUE ('구두/로퍼');
+INSERT INTO category (name)
+    VALUE ('워커/부츠');
+INSERT INTO category (name)
+    VALUE ('샌들/슬리퍼');
+INSERT INTO category (name)
+    VALUE ('스니커즈');
+INSERT INTO category (name)
+    VALUE ('기타');
