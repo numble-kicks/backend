5f19e86a25f82b6b307e7eefe3b3c6d9c8f988f4
chkim <cndghks0213@gmail.com>
2022-05-13 00:17:30 +0900

    refactor: 기존 직접구현 로그인 클래스 제거

--- src/main/java/numble/team4/shortformserver/member/auth/application/AuthService.java
+++ src/main/java/numble/team4/shortformserver/member/auth/application/AuthService.java
@@ -3,11 +3,7 @@ package numble.team4.shortformserver.member.auth.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.member.auth.application.dto.LoginResponse;
-import numble.team4.shortformserver.member.auth.application.membercreator.MemberCreatorFactory;
-import numble.team4.shortformserver.member.auth.application.membercreator.MemberCreatorFromSocialInfo;
 import numble.team4.shortformserver.member.auth.exception.JwtTokenExpiredException;
-import numble.team4.shortformserver.member.auth.infrastructure.OauthClient;
-import numble.team4.shortformserver.member.auth.infrastructure.OauthClientFactory;
 import numble.team4.shortformserver.member.auth.util.JwtTokenProvider;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.MemberRepository;
@@ -17,29 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AuthService {
 
-    private final OauthClientFactory oauthProviderFactory;
-    private final MemberCreatorFactory memberCreatorFactory;
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
-
-    public String findProviderRedirectUrl(String provider) {
-        OauthClient oauthClient = oauthProviderFactory.findOauthClient(provider);
-        return oauthClient.getRedirectUrl();
-    }
-
-    public LoginResponse signUpOrLogin(String code, String provider) {
-        OauthClient oauthClient = oauthProviderFactory.findOauthClient(provider);
-        MemberCreatorFromSocialInfo creator = memberCreatorFactory.findMemberCreator(provider);
-
-        Member member = creator.signUpOrLoginMember(oauthClient.getUserProfile(code));
-        return LoginResponse.of(member, jwtTokenProvider.createTokens(member));
-    }
 
     public LoginResponse renewToken(HttpServletRequest request) {
         String refreshToken = jwtTokenProvider.extractToken(request);
