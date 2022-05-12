1095330c1e8bfaffc8e1ff2d03f601d823f56184
jaeesu <wogmlsu@gmail.com>
2022-05-12 22:07:12 +0900

    feat: 사용자 이미지 등록 api 추가

--- src/main/java/numble/team4/shortformserver/member/member/ui/MemberController.java
+++ src/main/java/numble/team4/shortformserver/member/member/ui/MemberController.java
@@ -2,11 +2,14 @@ package numble.team4.shortformserver.member.member.ui;
 
 import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.common.dto.CommonResponse;
+import numble.team4.shortformserver.member.auth.util.LoginUser;
 import numble.team4.shortformserver.member.member.application.MemberService;
+import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.ui.dto.MemberInfoResponse;
 import numble.team4.shortformserver.video.application.VideoService;
 import numble.team4.shortformserver.video.dto.VideoListResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,19 +17,19 @@ import static numble.team4.shortformserver.member.member.ui.MemberResponseMessag
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/users/{memberId}")
+@RequestMapping("/v1/users")
 public class MemberController {
 
     private final MemberService memberService;
     private final VideoService videoService;
 
-    @GetMapping
+    @GetMapping("/{memberId}")
     public CommonResponse<MemberInfoResponse> findMemberInfo(@PathVariable Long memberId) {
         MemberInfoResponse res = memberService.getMemberInfo(memberId);
         return CommonResponse.of(res, GET_MEMBER_INFO.getMessage());
     }
 
-    @GetMapping("/videos")
+    @GetMapping("/{memberId}/videos")
     public CommonResponse<List<VideoListResponse>> findAllVideosByMember(@PathVariable Long memberId,
                                                                          @RequestParam(value = "last_video_id", required = false) Long videoId) {
         List<VideoListResponse> videos = videoService.findAllVideosByMember(memberId, videoId);
@@ -38,6 +41,12 @@ public class MemberController {
                                                                              @RequestParam(value = "last_video_id", required = false) Long videoId) {
         List<VideoListResponse> videos = videoService.findAllLikeVideosByMember(memberId, videoId);
         return CommonResponse.of(videos, GET_ALL_LIKE_VIDEOS_OF_MEMBER.getMessage());
+    }
+
+    @PutMapping("/image")
+    public CommonResponse updateProfileImage(@LoginUser Member member, MultipartFile file) {
+        memberService.saveProfileImage(member, file);
+        return CommonResponse.from(SAVE_PROFILE_IMAGE.getMessage());
     }
 
 
--- src/main/java/numble/team4/shortformserver/member/member/ui/MemberResponseMessage.java
+++ src/main/java/numble/team4/shortformserver/member/member/ui/MemberResponseMessage.java
@@ -9,7 +9,8 @@ public enum MemberResponseMessage {
 
     GET_ALL_VIDEOS_OF_MEMBER("회원의 영상 목록 조회 성공"),
     GET_ALL_LIKE_VIDEOS_OF_MEMBER("회원이 좋아요를 누른 영상 목록 조회 성공"),
-    GET_MEMBER_INFO("회원 정보 조회 성공");
+    GET_MEMBER_INFO("회원 정보 조회 성공"),
+    SAVE_PROFILE_IMAGE("회원 프로필 이미지 저장 성공");
 
     private final String message;
 }
