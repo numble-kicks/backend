087535b653af5ab74a5e901380bed133e3f1a8fe
chkim <cndghks0213@gmail.com>
2022-05-12 11:32:37 +0900

    feat: 채팅 메시지 엔티티 생성

---
+++ src/main/java/numble/team4/shortformserver/chat/domain/message/ChatMessage.java
@@ -0,0 +1,43 @@
+package numble.team4.shortformserver.chat.domain.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import numble.team4.shortformserver.chat.domain.room.ChatRoom;
+import numble.team4.shortformserver.common.domain.BaseTimeEntity;
+import numble.team4.shortformserver.member.member.domain.Member;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class ChatMessage extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    private String content;
+
+    private ChatMessage(Member member, ChatRoom chatRoom, String content) {
+        this.member = member;
+        this.chatRoom = chatRoom;
+        this.content = content;
+    }
+
+    public static ChatMessage of(Member member, ChatRoom chatRoom, String content) {
+        return new ChatMessage(member, chatRoom, content);
+    }
+}
