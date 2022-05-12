ad20e6d367ebffd1e36aed9abb3df8d15ca14fea
chkim <cndghks0213@gmail.com>
2022-05-12 23:58:23 +0900

    style: 불필요한 개행 제거

--- src/main/java/numble/team4/shortformserver/member/member/exception/NotAuthorException.java
+++ src/main/java/numble/team4/shortformserver/member/member/exception/NotAuthorException.java
@@ -3,5 +3,4 @@ package numble.team4.shortformserver.member.member.exception;
 import numble.team4.shortformserver.common.exception.BaseException;
 
 public class NotAuthorException extends BaseException {
-
 }
