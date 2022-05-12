e3efbed4bba3582b5ac35918c78cb34d9b8bf046
chkim <cndghks0213@gmail.com>
2022-05-12 11:39:27 +0900

    feat: 채팅 메시지 repository 생성

---
+++ src/main/java/numble/team4/shortformserver/chat/domain/message/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package numble.team4.shortformserver.chat.domain.message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByChatRoomId(Long id);
+}
