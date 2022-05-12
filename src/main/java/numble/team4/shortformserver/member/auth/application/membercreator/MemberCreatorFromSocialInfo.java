b712233cae05932f76e6ff202d3f1feab2c0f33b
chkim <cndghks0213@gmail.com>
2022-05-13 00:13:13 +0900

    refactor: 불필요한 소셜 로그인 구현 클래스 제거

--- src/main/java/numble/team4/shortformserver/member/auth/application/membercreator/MemberCreatorFromSocialInfo.java
+++
@@ -1,9 +0,0 @@
-package numble.team4.shortformserver.member.auth.application.membercreator;
-
-import numble.team4.shortformserver.member.auth.domain.OauthProvider;
-import numble.team4.shortformserver.member.member.domain.Member;
-
-public interface MemberCreatorFromSocialInfo {
-    Member signUpOrLoginMember(String response);
-    OauthProvider getProviderName();
-}
