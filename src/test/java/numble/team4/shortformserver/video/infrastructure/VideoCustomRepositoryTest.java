c4b0e43e6f82e2e626524067fe65b05a005c9a38
jaeesu <wogmlsu@gmail.com>
2022-05-12 13:14:40 +0900

    test: 사용자가 좋아요한 목록 조회 커스텀 레포지토리 테스트 추가

--- src/test/java/numble/team4/shortformserver/video/infrastructure/VideoCustomRepositoryTest.java
+++ src/test/java/numble/team4/shortformserver/video/infrastructure/VideoCustomRepositoryTest.java
@@ -1,20 +1,23 @@
 package numble.team4.shortformserver.video.infrastructure;
 
+import numble.team4.shortformserver.likevideo.domain.LikeVideo;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.Role;
 import numble.team4.shortformserver.testCommon.BaseDataJpaTest;
 import numble.team4.shortformserver.video.domain.Video;
 import numble.team4.shortformserver.video.domain.VideoRepository;
 import numble.team4.shortformserver.video.dto.VideoListResponse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -50,6 +53,14 @@ class VideoCustomRepositoryTest {
         }
     }
 
+    void createLikeVideo() {
+        List<Video> all = videoRepository.findAll();
+        for (int i = 0; i < all.size(); i++) {
+            LikeVideo likeVideo = LikeVideo.fromMemberAndVideo(member, all.get(i));
+            testEntityManager.persist(likeVideo);
+        }
+    }
+
     @Test
     @DisplayName("[성공] 사용자의 동영상 목록을 조회 (videoId가 null일 때)")
     void findAllByMemberAndMaxVideoId_returnListHasSizeLimitNum_success() {
@@ -82,4 +93,32 @@ class VideoCustomRepositoryTest {
             assertTrue(videos.get(i).getId() > videos.get(i + 1).getId());
         }
     }
+
+    static Stream<Long> valueSources() {
+        return Stream.of(null, 3L, 5L, 9L);
+    }
+
+    @ParameterizedTest
+    @DisplayName("[성공] 사용자가 좋아요한 동영상 목록을 조회")
+    @MethodSource("valueSources")
+    void findAllLikeVideoByMemberAndMaxVideoId_returnListHasSizeLimitNum_success(Long intVal) {
+        //given
+        createLikeVideo();
+        long count = 5;
+        if (intVal != null) {
+            count = videoRepository.findAll().stream().map(x -> x.getId())
+                    .filter(x -> x < intVal)
+                    .count();
+        }
+
+        //when
+        List<VideoListResponse> res = videoRepository.findAllLikeVideoByMemberAndMaxVideoId(member, intVal, 5);
+
+        //then
+        assertThat(res).hasSize((int) count);
+        for (int i = 0; i < res.size() - 1; i++) {
+            assertTrue(res.get(i).getId() > res.get(i + 1).getId());
+        }
+    }
+
 }
