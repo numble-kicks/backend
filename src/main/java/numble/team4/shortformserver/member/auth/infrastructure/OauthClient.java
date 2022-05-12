781ea8554084d8220f45b0d7114492e0abe1a323
chkim <cndghks0213@gmail.com>
2022-05-13 00:15:56 +0900

    refactor: 불필요한 social client 제거

--- src/main/java/numble/team4/shortformserver/member/auth/infrastructure/OauthClient.java
+++
@@ -1,9 +0,0 @@
-package numble.team4.shortformserver.member.auth.infrastructure;
-
-import numble.team4.shortformserver.member.auth.domain.OauthProvider;
-
-public interface OauthClient {
-    String getRedirectUrl();
-    String getUserProfile(String code);
-    OauthProvider getProviderName();
-}
