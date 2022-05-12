263b7e27bd02bc71fa77efe0a3dd9d303f9eefda
chkim <cndghks0213@gmail.com>
2022-05-13 01:03:40 +0900

    feat: 직접 구현한 소셜 로그인, 콜백 기능 제거

--- src/main/java/numble/team4/shortformserver/member/auth/ui/AuthController.java
+++ src/main/java/numble/team4/shortformserver/member/auth/ui/AuthController.java
@@ -6,14 +6,10 @@ import numble.team4.shortformserver.common.dto.CommonResponse;
 import numble.team4.shortformserver.member.auth.application.AuthService;
 import numble.team4.shortformserver.member.auth.application.dto.LoginResponse;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-import static numble.team4.shortformserver.member.auth.ui.AuthResponseMessage.LOGIN_SUCCESS;
 import static numble.team4.shortformserver.member.auth.ui.AuthResponseMessage.TOKEN_RENEW_SUCCESS;
 
 @Slf4j
@@ -22,16 +18,6 @@ import static numble.team4.shortformserver.member.auth.ui.AuthResponseMessage.TO
 public class AuthController {
 
     private final AuthService authService;
-
-    @GetMapping("/oauth/login")
-    public void oauthLogin(String provider, HttpServletResponse response) throws IOException {
-        response.sendRedirect(authService.findProviderRedirectUrl(provider));
-    }
-
-    @GetMapping("/oauth/callback")
-    public CommonResponse<LoginResponse> callback(@RequestParam String code, @RequestParam String state) {
-        return CommonResponse.of(authService.signUpOrLogin(code, state), LOGIN_SUCCESS.getMessage());
-    }
 
     @GetMapping("/renew")
     public CommonResponse<LoginResponse> oauthLogin(HttpServletRequest request) {
