bef34cc5ebab76fc1c1078c6757c1dd29fbd32a1
chkim <cndghks0213@gmail.com>
2022-05-12 11:43:21 +0900

    feat: 채팅방 생성 및 조회 기능 구현

---
+++ src/main/java/numble/team4/shortformserver/chat/application/ChatRoomService.java
@@ -0,0 +1,42 @@
+package numble.team4.shortformserver.chat.application;
+
+import lombok.RequiredArgsConstructor;
+import numble.team4.shortformserver.chat.application.dto.ChatRoomResponse;
+import numble.team4.shortformserver.chat.domain.room.ChatRoom;
+import numble.team4.shortformserver.chat.domain.room.ChatRoomRepository;
+import numble.team4.shortformserver.common.dto.CommonResponse;
+import numble.team4.shortformserver.common.dto.PageInfo;
+import numble.team4.shortformserver.member.member.domain.Member;
+import numble.team4.shortformserver.member.member.domain.MemberRepository;
+import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static numble.team4.shortformserver.chat.ui.ChatRoomResponseMessage.FIND_CHAT_ROOM;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void createChatRoom(Member buyer, Long sellerId) {
+        Member seller = memberRepository.findById(sellerId)
+                .orElseThrow(NotExistMemberException::new);
+        chatRoomRepository.findChatRoomByAllMember(buyer, seller)
+                        .orElseGet(() -> chatRoomRepository.save(ChatRoom.of(buyer, seller)));
+    }
+
+    public CommonResponse<List<ChatRoomResponse>> findChatRooms(Member member, Pageable pageable) {
+        Page<ChatRoomResponse> chatRooms = chatRoomRepository.findChatRoomByMember(member, pageable)
+                .map(ChatRoomResponse::from);
+        return CommonResponse.of(chatRooms.getContent(), PageInfo.from(chatRooms), FIND_CHAT_ROOM.getMessage());
+    }
+}
