29872fe14d0c7c97d663a220aee3190c0c432083
chkim <cndghks0213@gmail.com>
2022-05-13 00:17:02 +0900

    feat: 기존에 구현한 클래스를 예외처리에 사용하기 위해 설정 변경

--- src/main/java/numble/team4/shortformserver/common/config/SecurityConfig.java
+++ src/main/java/numble/team4/shortformserver/common/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.common.filter.JwtAuthenticationFilter;
 import numble.team4.shortformserver.common.filter.JwtExceptionHandleFilter;
+import numble.team4.shortformserver.member.auth.application.CustomOAuth2UserService;
+import numble.team4.shortformserver.member.auth.application.OAuth2SuccessHandler;
 import numble.team4.shortformserver.member.auth.util.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -23,6 +26,9 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final JwtTokenProvider jwtTokenProvider;
     private final ObjectMapper objectMapper;
 
@@ -39,8 +45,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/v1/videos/{videoId}/likes", "/v1/videos/likes/{likesId}").hasAnyRole(MEMBER.name(), ADMIN.name())
                 .antMatchers(HttpMethod.GET,"/v1/users/following/from", "/v1/users/following/to").permitAll()
-                .antMatchers("/oauth/**", "/ws-connection/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/oauth2/authorization/**", "/login/oauth2/code/**", "/ws-connection/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+
+                .and()
+                .oauth2Login()
+                .successHandler(oAuth2SuccessHandler)
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 
     @Bean
