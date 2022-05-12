34e34579950590230d27ff1b57a9bbe1964b373c
chkim <cndghks0213@gmail.com>
2022-05-12 17:23:02 +0900

    refactor: 불필요한 validation 제거

--- src/main/java/numble/team4/shortformserver/chat/ui/dto/ChatMessageRequest.java
+++ src/main/java/numble/team4/shortformserver/chat/ui/dto/ChatMessageRequest.java
@@ -3,18 +3,13 @@ package numble.team4.shortformserver.chat.ui.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @NoArgsConstructor
 public class ChatMessageRequest {
 
-    @NotBlank(message = "채팅방 ID를 입력해주세요.")
     private Long roomId;
 
-    @NotBlank(message = "유저 ID를 입력해주세요.")
     private Long userId;
 
-    @NotBlank(message = "메시지를 입력해주세요.")
     private String message;
 }
