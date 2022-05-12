294e6904d10f9db407d15082cca1a58147dd0665
seongho-joo <hazzyseong@gmail.com>
2022-05-12 17:22:36 +0900

    refactor: DDL, DML 분리

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -82,14 +82,3 @@ ALTER TABLE video
 
 ALTER TABLE category
     ADD CONSTRAINT UNIQUE (name);
-
-INSERT INTO category (name)
-    VALUE ('구두/로퍼');
-INSERT INTO category (name)
-    VALUE ('워커/부츠');
-INSERT INTO category (name)
-    VALUE ('샌들/슬리퍼');
-INSERT INTO category (name)
-    VALUE ('스니커즈');
-INSERT INTO category (name)
-    VALUE ('기타');
