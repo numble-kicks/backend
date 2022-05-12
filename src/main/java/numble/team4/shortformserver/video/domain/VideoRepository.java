a9455aba53b8f3731d89213aeace8c0a254bd631
seongho-joo <hazzyseong@gmail.com>
2022-05-12 15:28:39 +0900

    refactor: 사용자 영상 조회 제거

--- src/main/java/numble/team4/shortformserver/video/domain/VideoRepository.java
+++ src/main/java/numble/team4/shortformserver/video/domain/VideoRepository.java
@@ -1,13 +1,13 @@
 package numble.team4.shortformserver.video.domain;
 
-import numble.team4.shortformserver.member.member.domain.Member;
+import numble.team4.shortformserver.video.infrastructure.VideoCustomRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface VideoRepository extends JpaRepository<Video, Long> {
+public interface VideoRepository extends JpaRepository<Video, Long>, VideoCustomRepository {
 
     @Query("select v from Video v order by v.likeCount desc")
     Page<Video> findAllSortByLikeCount(Pageable pageable);
@@ -20,12 +20,6 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
 
     @Query("select v from Video v where v.viewCount < :hits order by v.viewCount desc")
     Page<Video> findAllSortByViewCount(@Param("hits") Long hits, Pageable pageable);
-
-    @Query("select v from Video v where v.member = :member order by v.createAt desc")
-    Page<Video> findAllVideosOfMember(@Param("member") Member member, Pageable pageable);
-
-    @Query("select v from Video v where v.member = :member and v.id < :videoId order by v.createAt desc")
-    Page<Video> findAllVideosOfMember(@Param("videoId") Long videoId, @Param("member") Member member, Pageable pageable);
 
     boolean existsById(Long id);
 }
