9342c965abc7714fefb74c05b0afc2c6681abe95
seongho-joo <hazzyseong@gmail.com>
2022-05-12 17:53:32 +0900

    refactor: 모든 영상 조회 메서드 통합

--- src/main/java/numble/team4/shortformserver/video/application/VideoService.java
+++ src/main/java/numble/team4/shortformserver/video/application/VideoService.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.aws.application.AmazonS3Uploader;
 import numble.team4.shortformserver.aws.dto.S3UploadDto;
 import numble.team4.shortformserver.member.member.domain.Member;
-import numble.team4.shortformserver.member.member.domain.MemberRepository;
-import numble.team4.shortformserver.member.member.exception.NotExistMemberException;
 import numble.team4.shortformserver.video.category.domain.Category;
 import numble.team4.shortformserver.video.category.domain.CategoryRepository;
 import numble.team4.shortformserver.video.category.exception.NotFoundCategoryException;
@@ -30,7 +28,6 @@ public class VideoService {
 
     private final VideoRepository videoRepository;
     private final CategoryRepository categoryRepository;
-    private final MemberRepository memberRepository;
     private final AmazonS3Uploader amazonS3Uploader;
 
     @Transactional
@@ -48,8 +45,7 @@ public class VideoService {
             category
         );
 
-        Video saveVideo = videoRepository.save(video);
-        return VideoResponse.from(saveVideo);
+        return VideoResponse.from(videoRepository.save(video));
     }
 
     @Transactional
@@ -64,6 +60,9 @@ public class VideoService {
         Category category = categoryRepository.findByName(videoUpdateRequest.getCategory())
             .orElseThrow(NotFoundCategoryException::new);
 
+
+
+        findVideo.validateAuthor(loggedInMember);
         findVideo.update(
             videoUpdateRequest.getTitle(),
             videoUpdateRequest.getDescription(),
@@ -75,11 +74,11 @@ public class VideoService {
     }
 
     @Transactional
-    public void deleteVideo(Long videoId, Member loggedMember) {
+    public void deleteVideo(Long videoId, Member loggedInMember) {
         Video findVideo = videoRepository.findById(videoId)
             .orElseThrow(NotExistVideoException::new);
 
-        findVideo.delete(loggedMember);
+        findVideo.validateAuthor(loggedInMember);
 
         amazonS3Uploader.deleteToS3(findVideo.getVideoUrl());
         amazonS3Uploader.deleteToS3(findVideo.getThumbnailUrl());
@@ -87,52 +86,28 @@ public class VideoService {
         videoRepository.delete(findVideo);
     }
 
+    @Transactional
     public VideoResponse findVideoById(Long videoId) {
         Video findVideo = videoRepository.findById(videoId).orElseThrow(NotExistVideoException::new);
         findVideo.increaseViewCount();
-
         return VideoResponse.from(findVideo);
     }
 
-    public Page<VideoResponse> findAllVideosOfMember(Long memberId, Long videoId, Pageable pageable) {
-        Member member = memberRepository.findById(memberId)
-            .orElseThrow(NotExistMemberException::new);
-
+    public Page<VideoResponse> findAllVideos(Long videoId, String sortBy, Pageable pageable) {
         Page<VideoResponse> videos;
         if (Objects.isNull(videoId)) {
-            videos = videoRepository.findAllVideosOfMember(member, pageable).map(VideoResponse::from);
+            videos = videoRepository.findAllVideos(null, sortBy, pageable).map(VideoResponse::from);
         } else {
-            videos = videoRepository.findAllVideosOfMember(videoId, member, pageable).map(VideoResponse::from);
+            String cursor;
+            if (sortBy.equals("hits")) {
+                cursor = videoRepository.findById(videoId)
+                    .orElseThrow(NotExistVideoException::new).getHitsCursor();
+            } else {
+                cursor = videoRepository.findById(videoId)
+                    .orElseThrow(NotExistVideoException::new).getLikesCursor();
+            }
+            videos = videoRepository.findAllVideos(cursor, sortBy, pageable).map(VideoResponse::from);
         }
-
-        return videos;
-    }
-
-    public Page<VideoResponse> findAllVideosSortByLikes(Long videoId, Pageable pageable) {
-        Page<VideoResponse> videos;
-
-        if (Objects.isNull(videoId)) {
-            videos = videoRepository.findAllSortByLikeCount(pageable).map(VideoResponse::from);
-        } else {
-            Long likes = videoRepository.findById(videoId)
-                .orElseThrow(NotExistVideoException::new).getLikeCount();
-            videos = videoRepository.findAllSortByLikeCount(likes, pageable).map(VideoResponse::from);
-        }
-
-        return videos;
-    }
-
-    public Page<VideoResponse> findAllVideosSortByHits(Long videoId, Pageable pageable) {
-        Page<VideoResponse> videos;
-
-        if (Objects.isNull(videoId)) {
-            videos = videoRepository.findAllSortByViewCount(pageable).map(VideoResponse::from);
-        } else {
-            Long hits = videoRepository.findById(videoId)
-                .orElseThrow(NotExistVideoException::new).getViewCount();
-            videos = videoRepository.findAllSortByViewCount(hits, pageable).map(VideoResponse::from);
-        }
-
         return videos;
     }
 }
