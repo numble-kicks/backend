d58ffc83ecb3f6208ed1485fda6ca69294b26ff0
jaeesu <wogmlsu@gmail.com>
2022-05-13 11:37:25 +0900

    refactor: 닉네임 수정 요청값 생성자 추가

--- src/main/java/numble/team4/shortformserver/member/member/ui/dto/MemberNameUpdateRequest.java
+++ src/main/java/numble/team4/shortformserver/member/member/ui/dto/MemberNameUpdateRequest.java
@@ -1,10 +1,12 @@
 package numble.team4.shortformserver.member.member.ui.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@AllArgsConstructor
 public class MemberNameUpdateRequest {
 
     @NotBlank(message = "변경하실 닉네임을 입력해주세요.")
