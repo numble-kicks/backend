dfffeed064433fcf03f9809cc2f95ec462f138f4
jaeesu <wogmlsu@gmail.com>
2022-05-13 15:11:58 +0900

    refactor: json 변환 오류로 request 생성자 수정

--- src/main/java/numble/team4/shortformserver/member/member/ui/dto/MemberEmailRequest.java
+++ src/main/java/numble/team4/shortformserver/member/member/ui/dto/MemberEmailRequest.java
@@ -2,16 +2,20 @@ package numble.team4.shortformserver.member.member.ui.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import static lombok.AccessLevel.PROTECTED;
+
 @Getter
+@NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class MemberEmailRequest {
 
     @NotBlank(message = "이메일은 빈 값이 허용되지 않습니다.")
     @Email(message = "잘못된 이메일 형식입니다.")
-    private final String email;
+    private String email;
 
 }
