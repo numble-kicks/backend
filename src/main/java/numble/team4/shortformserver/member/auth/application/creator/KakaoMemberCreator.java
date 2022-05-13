5aac38803e91f115edf9065529c31b5641be79d7
chkim <cndghks0213@gmail.com>
2022-05-13 18:28:07 +0900

    feat: 카카오톡 회원 정보를 바탕으로 회원가입 또는 로그인 기능 구현

--- src/main/java/numble/team4/shortformserver/member/auth/application/creator/KakaoMemberCreator.java
+++ src/main/java/numble/team4/shortformserver/member/auth/application/creator/KakaoMemberCreator.java
@@ -1,7 +1,10 @@
 package numble.team4.shortformserver.member.auth.application.creator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.member.auth.domain.OauthProvider;
+import numble.team4.shortformserver.member.auth.exception.KakaoLoginFailException;
 import numble.team4.shortformserver.member.member.domain.Member;
 import numble.team4.shortformserver.member.member.domain.MemberRepository;
 import org.springframework.stereotype.Component;
@@ -14,28 +17,40 @@ import static numble.team4.shortformserver.member.auth.domain.OauthProvider.KAKA
 import static numble.team4.shortformserver.member.member.domain.Role.MEMBER;
 
 @Component
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class KakaoMemberCreator implements MemberCreator {
 
-    private final MemberRepository memberRepository;
+    private static final String KAKAO_ACCOUNT = "kakao_account";
+    private static final String PROPERTIES = "properties";
+    private static final String NICKNAME = "nickname";
+    private static final String EMAIL = "email";
+    private static final String ID = "id";
 
+    private final MemberRepository memberRepository;
+    private final ObjectMapper objectMapper;
+
+    @Transactional
     @Override
-    public Member createMemberFromAttributes(Map<String, Object> attribute) {
+    public Member signUpOrLoginMember(String response) {
+        Map<String, Object> attribute = extractAttributeFromResponse(response);
         Long userId = (Long) attribute.get("id");
         return memberRepository.findByUserIdAndProvider(userId, KAKAO)
                 .orElseGet(() -> saveMember(attribute));
     }
 
-    @Override
-    public OauthProvider getProvider() {
-        return KAKAO;
+    private Map<String, Object> extractAttributeFromResponse(String response) {
+        try {
+            return (Map<String, Object>) objectMapper.readValue(response, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new KakaoLoginFailException();
+        }
     }
 
     private Member saveMember(Map<String, Object> attribute) {
         String email = extractEmailFromAttribute(attribute);
         return memberRepository.save(new Member(
-                (Long) attribute.get("id"),
+                (Long) attribute.get(ID),
                 email,
                 extractNameFromAttribute(attribute),
                 MEMBER,
@@ -46,14 +61,19 @@ public class KakaoMemberCreator implements MemberCreator {
 
     private String extractEmailFromAttribute(Map<String, Object> attribute) {
         try {
-            return (String) ((Map<String, Object>) attribute.get("kakao_account"))
-                    .get("email");
+            return (String) ((Map<String, Object>) attribute.get(KAKAO_ACCOUNT))
+                    .get(EMAIL);
         } catch (Exception e) {
             return null;
         }
     }
 
     private String extractNameFromAttribute(Map<String, Object> attribute) {
-        return (String) ((Map<String, Object>) attribute.get("properties")).get("nickname");
+        return (String) ((Map<String, Object>) attribute.get(PROPERTIES)).get(NICKNAME);
+    }
+
+    @Override
+    public OauthProvider getProviderName() {
+        return KAKAO;
     }
 }
