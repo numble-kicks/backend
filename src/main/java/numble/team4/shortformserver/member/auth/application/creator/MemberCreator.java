82034e3727c583952c7f22a267b1bbdbde5c6584
chkim <cndghks0213@gmail.com>
2022-05-13 18:27:37 +0900

    refactor: 소셜 로그인 제공사로부터 받은 응답으로 회원가입 또는 로그인 하는 기능을 가진 인터페이스

--- src/main/java/numble/team4/shortformserver/member/auth/application/creator/MemberCreator.java
+++ src/main/java/numble/team4/shortformserver/member/auth/application/creator/MemberCreator.java
@@ -3,9 +3,7 @@ package numble.team4.shortformserver.member.auth.application.creator;
 import numble.team4.shortformserver.member.auth.domain.OauthProvider;
 import numble.team4.shortformserver.member.member.domain.Member;
 
-import java.util.Map;
-
 public interface MemberCreator {
-    Member createMemberFromAttributes(Map<String, Object> attribute);
-    OauthProvider getProvider();
+    Member signUpOrLoginMember(String response);
+    OauthProvider getProviderName();
 }
