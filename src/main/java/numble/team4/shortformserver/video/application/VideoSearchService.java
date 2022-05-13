bfcbe95810e5637e11adf6a7f49b68def8e82788
seongho-joo <hazzyseong@gmail.com>
2022-05-13 21:27:19 +0900

    feat: 검색 페이지에서 필요한 서비스 구현 * top10 영상 가져오기 * 키워드로 영상 검색

---
+++ src/main/java/numble/team4/shortformserver/video/application/VideoSearchService.java
@@ -0,0 +1,32 @@
+package numble.team4.shortformserver.video.application;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import numble.team4.shortformserver.video.domain.VideoRepository;
+import numble.team4.shortformserver.video.dto.VideoListResponse;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class VideoSearchService {
+
+    private final VideoRepository videoRepository;
+
+    public List<VideoListResponse> searchByKeyword(Long lastId, String keyword, String sortBy) {
+        return videoRepository.searchVideoByKeyword(lastId, keyword, sortBy)
+            .stream()
+            .map(VideoListResponse::from)
+            .collect(Collectors.toList());
+    }
+
+    public List<VideoListResponse> getVideoTop10(String sortBy) {
+        return videoRepository.getVideoTop10(sortBy)
+            .stream()
+            .map(VideoListResponse::from)
+            .collect(Collectors.toList());
+    }
+
+}
