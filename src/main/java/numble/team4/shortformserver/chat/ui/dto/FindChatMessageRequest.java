a6dd21b6b2a1beb5a020969054d527f8073c366a
chkim <cndghks0213@gmail.com>
2022-05-14 23:28:11 +0900

    refactor: messageId에 대한 제약조건 제거

--- src/main/java/numble/team4/shortformserver/chat/ui/dto/FindChatMessageRequest.java
+++ src/main/java/numble/team4/shortformserver/chat/ui/dto/FindChatMessageRequest.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class FindChatMessageRequest {
 
-    @NotNull(message = "메시지 아이디를 입력해주세요.")
     private Long messageId;
 
     @NotNull(message = "ChatMessage 응답 크기를 입력해주세요.")
