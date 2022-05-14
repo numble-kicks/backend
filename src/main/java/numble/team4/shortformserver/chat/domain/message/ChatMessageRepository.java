ae1c7a409fff5bd2c66bfe32437f020ad44defbd
chkim <cndghks0213@gmail.com>
2022-05-14 22:42:44 +0900

    feat: ChatMessageCustomRepository 추가

--- src/main/java/numble/team4/shortformserver/chat/domain/message/ChatMessageRepository.java
+++ src/main/java/numble/team4/shortformserver/chat/domain/message/ChatMessageRepository.java
@@ -1,9 +1,7 @@
 package numble.team4.shortformserver.chat.domain.message;
 
+import numble.team4.shortformserver.chat.infrastructure.message.ChatMessageCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findByChatRoomId(Long id);
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>, ChatMessageCustomRepository {
 }
