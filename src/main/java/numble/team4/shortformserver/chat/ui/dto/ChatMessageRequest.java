c5c566dd4e9d77812a9957cbefd132d0e645a72b
chkim <cndghks0213@gmail.com>
2022-05-14 22:35:48 +0900

    refactor: 불필요한 roomId 제거

--- src/main/java/numble/team4/shortformserver/chat/ui/dto/ChatMessageRequest.java
+++ src/main/java/numble/team4/shortformserver/chat/ui/dto/ChatMessageRequest.java
@@ -3,13 +3,16 @@ package numble.team4.shortformserver.chat.ui.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class ChatMessageRequest {
 
-    private Long roomId;
-
+    @NotNull(message = "유저 ID를 입력해주세요.")
     private Long userId;
 
+    @NotBlank(message = "메시지를 입력해주세요.")
     private String message;
 }
