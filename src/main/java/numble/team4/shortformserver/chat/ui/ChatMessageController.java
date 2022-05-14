acf7a03d5a133c721768e24cef0c628b4de63c07
chkim <cndghks0213@gmail.com>
2022-05-14 22:45:04 +0900

    refactor: 요청 url 경로 변경 및 message mapping pathVariable 어노테이션 추가

--- src/main/java/numble/team4/shortformserver/chat/ui/ChatMessageController.java
+++ src/main/java/numble/team4/shortformserver/chat/ui/ChatMessageController.java
@@ -5,14 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.chat.application.ChatMessageService;
 import numble.team4.shortformserver.chat.application.dto.ChatMessageResponse;
 import numble.team4.shortformserver.chat.ui.dto.ChatMessageRequest;
+import numble.team4.shortformserver.chat.ui.dto.FindChatMessageRequest;
 import numble.team4.shortformserver.common.dto.CommonResponse;
 import numble.team4.shortformserver.member.auth.util.LoginUser;
 import numble.team4.shortformserver.member.member.domain.Member;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static numble.team4.shortformserver.chat.ui.ChatRoomResponseMessage.FIND_MESSAGE;
@@ -25,14 +30,17 @@ public class ChatMessageController {
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final ChatMessageService chatMessageService;
 
-    @MessageMapping("/messages")
-    public void sendMessage(ChatMessageRequest request) {
-        chatMessageService.saveMessage(request);
-        simpMessagingTemplate.convertAndSend("/sub/rooms/" + request.getRoomId(), request.getMessage());
+    @MessageMapping("/rooms/{roomId}")
+    public void sendMessage(@DestinationVariable Long roomId, @Valid @RequestBody ChatMessageRequest request) {
+        chatMessageService.saveMessage(roomId, request);
+        simpMessagingTemplate.convertAndSend("/sub/rooms/" + roomId, request.getMessage());
     }
 
-    @GetMapping("/messages")
-    public CommonResponse<List<ChatMessageResponse>> findAllChats(@LoginUser Member member, Long roomId) {
-        return CommonResponse.of(chatMessageService.findAllChatMessages(member, roomId), FIND_MESSAGE.getMessage());
+    @GetMapping("/rooms/{roomId}/messages")
+    public CommonResponse<List<ChatMessageResponse>> findAllChats(
+            @LoginUser Member member,
+            @PathVariable Long roomId,
+            @Valid FindChatMessageRequest request) {
+        return CommonResponse.of(chatMessageService.findAllChatMessages(member, roomId, request), FIND_MESSAGE.getMessage());
     }
 }
