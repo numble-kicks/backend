fefa2e28bc14d2860246e65c49799d9068b0121d
chkim <cndghks0213@gmail.com>
2022-05-14 22:33:27 +0900

    feat: 무한 스크롤을 위해 id를 기준으로 최신 message를 조회하는 기능 구현

---
+++ src/main/java/numble/team4/shortformserver/chat/infrastructure/message/ChatMessageCustomRepositoryImpl.java
@@ -0,0 +1,35 @@
+package numble.team4.shortformserver.chat.infrastructure.message;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import numble.team4.shortformserver.chat.domain.message.ChatMessage;
+import numble.team4.shortformserver.chat.domain.room.ChatRoom;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Objects;
+
+import static numble.team4.shortformserver.chat.domain.message.QChatMessage.chatMessage;
+
+@RequiredArgsConstructor
+public class ChatMessageCustomRepositoryImpl implements ChatMessageCustomRepository {
+
+    private final JPAQueryFactory factory;
+
+    @Override
+    public List<ChatMessage> searchLastMessages(Long id, ChatRoom chatRoom, Pageable pageable) {
+        return factory.selectFrom(chatMessage)
+                .where(lessThan(id))
+                .orderBy(chatMessage.id.desc())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private BooleanExpression lessThan(Long id) {
+        if (Objects.isNull(id)) {
+            return chatMessage.id.lt(Long.MAX_VALUE);
+        }
+        return chatMessage.id.lt(id);
+    }
+}
