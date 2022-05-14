d7712e51e2427575c37ab3e4fefdff76180ee1f3
chkim <cndghks0213@gmail.com>
2022-05-14 22:41:39 +0900

    feat: customRepository 추가

--- src/main/java/numble/team4/shortformserver/chat/domain/room/ChatRoomRepository.java
+++ src/main/java/numble/team4/shortformserver/chat/domain/room/ChatRoomRepository.java
@@ -1,5 +1,6 @@
 package numble.team4.shortformserver.chat.domain.room;
 
+import numble.team4.shortformserver.chat.infrastructure.room.ChatRoomCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomCustomRepository {
