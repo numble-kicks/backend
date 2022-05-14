70ce7b8c48ee37018edbc694d61c9807ba675afa
seongho-joo <hazzyseong@gmail.com>
2022-05-15 01:08:38 +0900

    refactor: likeDescription, likeTitle 메서드 제거

--- src/main/java/numble/team4/shortformserver/video/infrastructure/VideoCustomRepositoryImpl.java
+++ src/main/java/numble/team4/shortformserver/video/infrastructure/VideoCustomRepositoryImpl.java
@@ -24,8 +24,8 @@ public class VideoCustomRepositoryImpl implements VideoCustomRepository {
     public List<Video> searchVideoByKeyword(Long lastId, String keyword, String sortBy) {
         return factory
             .selectFrom(video)
-            .where(likeTitle(keyword)
-                    .or(likeDescription(keyword)),
+            .where(video.description.contains(keyword)
+                    .or(video.title.contains(keyword)),
                 cursorIsLessThan(sortBy, lastId))
             .orderBy(videoSort(sortBy), video.id.desc())
             .limit(LIMIT)
@@ -41,21 +41,14 @@ public class VideoCustomRepositoryImpl implements VideoCustomRepository {
             .fetch();
     }
 
-    private BooleanExpression likeDescription(String keyword) {
-        return video.description.contains(keyword);
-    }
-
-    private BooleanExpression likeTitle(String keyword) {
-        return video.title.contains(keyword);
-    }
-
     private BooleanExpression cursorIsLessThan(String sort, Long cursorId) {
         if (Objects.isNull(cursorId)) {
             return null;
         }
 
         if (sort.equals("hits")) {
-            String cursor = String.format("%010d", getHits(cursorId)) + String.format("%010d", cursorId);
+            String cursor =
+                String.format("%010d", getHits(cursorId)) + String.format("%010d", cursorId);
             return convertToUniqueValue().lt(cursor);
         }
 
@@ -63,16 +56,17 @@ public class VideoCustomRepositoryImpl implements VideoCustomRepository {
     }
 
     /**
-     * 조회수가 같다면 같은 조회수는 제외된다.
-     * 따라서 조회수와 id를 활용해 공식을 만들어 Unique한 값을 생성하고 비교를 해야된다.
+     * 조회수가 같다면 같은 조회수는 제외된다. 따라서 조회수와 id를 활용해 공식을 만들어 Unique한 값을 생성하고 비교를 해야된다.
      */
     private StringExpression convertToUniqueValue() {
-        return lpad(video.viewCount.stringValue(), 10, '0').concat(lpad(video.id.stringValue(), 10, '0'));
+        return lpad(video.viewCount.stringValue(), 10, '0').concat(
+            lpad(video.id.stringValue(), 10, '0'));
     }
 
     private Long getHits(Long cursorId) {
         return Objects
-            .requireNonNull(factory.selectFrom(video).where(video.id.eq(cursorId)).fetchOne()).getViewCount();
+            .requireNonNull(factory.selectFrom(video).where(video.id.eq(cursorId)).fetchOne())
+            .getViewCount();
     }
 
     private OrderSpecifier<?> videoSort(String sortBy) {
