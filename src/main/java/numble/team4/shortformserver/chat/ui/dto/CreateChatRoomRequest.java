d04ee89df81369b10b05e6f699525f5b0590b55c
chkim <cndghks0213@gmail.com>
2022-05-14 23:28:33 +0900

    fix: Long type이므로 NotBlank대신 NotNull을 적용

--- src/main/java/numble/team4/shortformserver/chat/ui/dto/CreateChatRoomRequest.java
+++ src/main/java/numble/team4/shortformserver/chat/ui/dto/CreateChatRoomRequest.java
@@ -1,14 +1,17 @@
 package numble.team4.shortformserver.chat.ui.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class CreateChatRoomRequest {
 
-    @NotBlank(message = "판매자의 ID를 입력해주세요.")
+    @NotNull(message = "판매자의 ID를 입력해주세요.")
     private Long sellerId;
 }
