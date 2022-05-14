a3b17037a33f98e6b8b3c454eb3ec8a9711281f3
seongho-joo <hazzyseong@gmail.com>
2022-05-15 02:46:15 +0900

    feat: Query parameter DTO 생성

--- src/main/java/numble/team4/shortformserver/video/ui/VideoSearchController.java
+++ src/main/java/numble/team4/shortformserver/video/ui/VideoSearchController.java
@@ -8,33 +8,38 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.common.dto.CommonResponse;
 import numble.team4.shortformserver.video.application.VideoSearchService;
+import numble.team4.shortformserver.video.dto.VideoListRequest;
 import numble.team4.shortformserver.video.dto.VideoListResponse;
+import numble.team4.shortformserver.video.dto.VideoSearchRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/search")
+@RequestMapping("/v1/videos")
 public class VideoSearchController {
 
     private final VideoSearchService videoSearchService;
 
-    @GetMapping("/videos")
-    public CommonResponse<List<VideoListResponse>> searchVideoByKeyword(
-        @RequestParam String keyword,
-        @RequestParam(required = false) Long lastId,
-        @RequestParam(required = false) String sortBy
-    ) {
-        return CommonResponse.of(videoSearchService.searchByKeyword(lastId, keyword, sortBy),
+    @GetMapping("/keyword-condition")
+    public CommonResponse<List<VideoListResponse>> searchByKeyword(
+        @ModelAttribute VideoSearchRequest request) {
+
+        return CommonResponse.of(videoSearchService.searchByKeyword(
+                request.getLastId(),
+                request.getKeyword(),
+                request.getSortBy()
+            ),
             GET_VIDEO_LIST_BY_KEYWORD.getMessage());
     }
 
-    @GetMapping
-    public CommonResponse<List<VideoListResponse>> getTopVideo(@RequestParam String sortBy, @RequestParam(required = false) Integer limitNum) {
-        return CommonResponse.of(videoSearchService.getTopVideo(sortBy, 10),
+    @GetMapping("/status-condition")
+    public CommonResponse<List<VideoListResponse>> getTopVideo(
+        @ModelAttribute VideoListRequest request) {
+        return CommonResponse.of(videoSearchService.getTopVideo(request.getSortBy(), 10),
             GET_VIDEO_TOP_10.getMessage());
     }
 }
--- src/test/java/numble/team4/shortformserver/video/integration/VideoSearchIntegrationTest.java
+++ src/test/java/numble/team4/shortformserver/video/integration/VideoSearchIntegrationTest.java
@@ -13,7 +13,9 @@ import numble.team4.shortformserver.video.category.domain.CategoryRepository;
 import numble.team4.shortformserver.video.category.exception.NotFoundCategoryException;
 import numble.team4.shortformserver.video.domain.Video;
 import numble.team4.shortformserver.video.domain.VideoRepository;
+import numble.team4.shortformserver.video.dto.VideoListRequest;
 import numble.team4.shortformserver.video.dto.VideoListResponse;
+import numble.team4.shortformserver.video.dto.VideoSearchRequest;
 import numble.team4.shortformserver.video.ui.VideoSearchController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -109,7 +111,7 @@ class VideoSearchIntegrationTest {
         Object[] values = ids.stream().sorted(Collections.reverseOrder()).toArray();
 
         // when
-        List<VideoListResponse> res = videoSearchController.searchVideoByKeyword(keyword, null, null)
+        List<VideoListResponse> res = videoSearchController.searchByKeyword(new VideoSearchRequest(keyword, null, null))
             .getData();
 
         // then
@@ -138,8 +140,7 @@ class VideoSearchIntegrationTest {
         };
 
         // when
-        List<VideoListResponse> data = videoSearchController.searchVideoByKeyword(keyword,
-            ids.get(10), null).getData();
+        List<VideoListResponse> data = videoSearchController.searchByKeyword(new VideoSearchRequest(keyword, ids.get(10), null)).getData();
 
         // then
         assertThat(data)
@@ -169,8 +170,7 @@ class VideoSearchIntegrationTest {
         };
 
         // when
-        List<VideoListResponse> lastId가_null = videoSearchController.searchVideoByKeyword(keyword, null,
-            "hits").getData();
+        List<VideoListResponse> lastId가_null = videoSearchController.searchByKeyword(new VideoSearchRequest(keyword, null, "hits")).getData();
 
         // then
         assertThat(lastId가_null)
@@ -191,8 +191,7 @@ class VideoSearchIntegrationTest {
         };
 
         // when
-        List<VideoListResponse> lastId는8 = videoSearchController.searchVideoByKeyword(keyword, ids.get(7),
-            "hits").getData();
+        List<VideoListResponse> lastId는8 = videoSearchController.searchByKeyword(new VideoSearchRequest(keyword, ids.get(7), "hits")).getData();
 
         // then
         assertThat(lastId는8)
@@ -209,8 +208,7 @@ class VideoSearchIntegrationTest {
         Object[] value = new Object[] {ids.get(2), ids.get(1), ids.get(4)};
 
         // when
-        List<VideoListResponse> 정렬_순서_325 = videoSearchController.searchVideoByKeyword(keyword, ids.get(0),
-            "hits").getData();
+        List<VideoListResponse> 정렬_순서_325 = videoSearchController.searchByKeyword(new VideoSearchRequest(keyword, null, "hits")).getData();
 
         // then
         assertThat(정렬_순서_325)
@@ -250,8 +248,8 @@ class VideoSearchIntegrationTest {
         };
 
         // when
-        List<VideoListResponse> hits = videoSearchController.getTopVideo("hits", 10).getData();
-        List<VideoListResponse> likes = videoSearchController.getTopVideo("likes", 10).getData();
+        List<VideoListResponse> hits = videoSearchController.getTopVideo(new VideoListRequest("hits", 10)).getData();
+        List<VideoListResponse> likes = videoSearchController.getTopVideo(new VideoListRequest("likes", 10)).getData();
 
         // then
         assertThat(hits)
