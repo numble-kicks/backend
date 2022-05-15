41d1273fc06e9b3f432cf921f81a07c5b7e31a70
seongho-joo <hazzyseong@gmail.com>
2022-05-15 17:31:41 +0900

    feat: 회원 DTO 생성

---
+++ src/main/java/numble/team4/shortformserver/member/member/dto/MemberInfoResponse.java
@@ -0,0 +1,21 @@
+package numble.team4.shortformserver.member.member.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import numble.team4.shortformserver.member.member.domain.Member;
+
+@Getter
+@AllArgsConstructor
+public class MemberInfoResponse {
+    private Long id;
+
+    private String name;
+
+    @JsonProperty("profile_image_url")
+    private String profileImageUrl;
+
+    public static MemberInfoResponse from(Member member) {
+        return new MemberInfoResponse(member.getId(), member.getName(), member.getProfileImageUrl());
+    }
+}
