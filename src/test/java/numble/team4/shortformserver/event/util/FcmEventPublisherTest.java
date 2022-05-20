fe1ec0039e97323217e415367912e3890f94693b
jaeesu <wogmlsu@gmail.com>
2022-05-21 03:35:03 +0900

    test: fcm 이벤트 리스너 테스트 토큰 세팅 추가

--- src/test/java/numble/team4/shortformserver/event/util/FcmEventPublisherTest.java
+++ src/test/java/numble/team4/shortformserver/event/util/FcmEventPublisherTest.java
@@ -1,12 +1,13 @@
 package numble.team4.shortformserver.event.util;
 
 import numble.team4.shortformserver.event.domain.FcmEventDomain;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("local")
 @SpringBootTest
@@ -14,6 +15,19 @@ class FcmEventPublisherTest {
 
     @Autowired
     FcmEventPublisher publisher;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @BeforeEach
+    void init() {
+        stringRedisTemplate.opsForValue().set("13", "testFcmToken");
+    }
+
+    @AfterEach
+    void close() {
+        stringRedisTemplate.delete("13");
+    }
 
     @Test
     public void test() {
