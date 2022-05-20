a4a1df7caacd2c392e1a894ec35229d74aa1b6d7
jaeesu <wogmlsu@gmail.com>
2022-05-21 03:10:17 +0900

    feat: fcm 알림 push 서비스 추가

---
+++ src/main/java/numble/team4/shortformserver/notification/application/FcmPushService.java
@@ -0,0 +1,27 @@
+package numble.team4.shortformserver.notification.application;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import lombok.RequiredArgsConstructor;
+import numble.team4.shortformserver.event.domain.FcmEventDomain;
+import numble.team4.shortformserver.notification.domain.FcmMessage;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FcmPushService {
+
+    public static final FirebaseMessaging instance = FirebaseMessaging.getInstance();
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void sendMessage(Long memberId, Long destinationId, FcmEventDomain domain) {
+        Message message = FcmMessage.makeFcmMessage(memberId, domain, getFcmToken(destinationId));
+        instance.sendAsync(message);
+    }
+
+    private String getFcmToken(Long memberId) {
+        return stringRedisTemplate.opsForValue().get(memberId);
+    }
+
+}
