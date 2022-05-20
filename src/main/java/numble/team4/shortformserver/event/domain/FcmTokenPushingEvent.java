21a39c6ffa1529bdc491ba9f4c715a849e87dcaa
jaeesu <wogmlsu@gmail.com>
2022-05-21 02:26:46 +0900

    fix: 필드명 수정

--- src/main/java/numble/team4/shortformserver/event/domain/FcmTokenPushingEvent.java
+++ src/main/java/numble/team4/shortformserver/event/domain/FcmTokenPushingEvent.java
@@ -4,12 +4,14 @@ import org.springframework.context.ApplicationEvent;
 
 public class FcmTokenPushingEvent extends ApplicationEvent {
 
-    private Long memberId;
+    private Long currentMemberId;
+    private Long destinationMemberId;
     private FcmEventDomain domain;
 
-    public FcmTokenPushingEvent(Object source, Long memberId, FcmEventDomain domain) {
+    public FcmTokenPushingEvent(Object source, Long currentMemberId, Long destinationMemberId, FcmEventDomain domain) {
         super(source);
-        this.memberId = memberId;
+        this.currentMemberId = currentMemberId;
+        this.destinationMemberId = destinationMemberId;
         this.domain = domain;
     }
 }
