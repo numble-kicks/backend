da2e7ef272138236fbca3f59e8e0b392174956ea
jaeesu <wogmlsu@gmail.com>
2022-05-21 11:59:09 +0900

    feat: NotificationService 파일명 수정 및 토큰 로직과 분리

--- src/main/java/numble/team4/shortformserver/event/util/FcmEventListener.java
+++ src/main/java/numble/team4/shortformserver/event/util/FcmEventListener.java
@@ -3,7 +3,7 @@ package numble.team4.shortformserver.event.util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import numble.team4.shortformserver.event.domain.FcmTokenPushingEvent;
-import numble.team4.shortformserver.notification.application.FcmPushService;
+import numble.team4.shortformserver.notification.application.NotificationService;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +12,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class FcmEventListener implements ApplicationListener<FcmTokenPushingEvent> {
 
-    private final FcmPushService fcmPushService;
+    private final NotificationService notificationService;
 
     @Override
     public void onApplicationEvent(FcmTokenPushingEvent event) {
         log.info("generate Fcm token pushing event");
-        fcmPushService.sendMessage(event.getCurrentMemberId(), event.getDestinationMemberId(), event.getDomain());
+        notificationService.sendMessage(event.getCurrentMemberId(), event.getDestinationMemberId(), event.getDomain());
     }
 }
--- src/main/java/numble/team4/shortformserver/notification/application/NotificationService.java
+++ src/main/java/numble/team4/shortformserver/notification/application/NotificationService.java
@@ -5,23 +5,18 @@ import com.google.firebase.messaging.Message;
 import lombok.RequiredArgsConstructor;
 import numble.team4.shortformserver.event.domain.FcmEventDomain;
 import numble.team4.shortformserver.notification.domain.FcmMessage;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class FcmPushService {
+public class NotificationService {
 
     public final FirebaseMessaging instance;
-    private final StringRedisTemplate stringRedisTemplate;
+    private final FcmTokenService fcmTokenService;
 
     public void sendMessage(Long memberId, Long destinationId, FcmEventDomain domain) {
-        Message message = FcmMessage.makeFcmMessage(memberId, domain, getFcmToken(destinationId));
+        Message message = FcmMessage.makeFcmMessage(memberId, domain, fcmTokenService.getFcmToken(destinationId));
         instance.sendAsync(message);
-    }
-
-    private String getFcmToken(Long memberId) {
-        return stringRedisTemplate.opsForValue().get(String.valueOf(memberId));
     }
 
 }
