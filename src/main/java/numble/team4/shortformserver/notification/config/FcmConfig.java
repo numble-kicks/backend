0228e968d20b8a52766291c03964c0411d728ca8
jaeesu <wogmlsu@gmail.com>
2022-05-23 00:34:59 +0900

    fix: firebaseApp이 두 번 호출되는 버그 수정

--- src/main/java/numble/team4/shortformserver/notification/config/FcmConfig.java
+++ src/main/java/numble/team4/shortformserver/notification/config/FcmConfig.java
@@ -4,44 +4,50 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import numble.team4.shortformserver.notification.domain.FcmConfigEnvironment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class FcmConfig {
 
-    @Value("${firebase.config}")
-    private String configPath;
+    private final FcmConfigEnvironment config;
+    private static final String firebaseAppName = "kicks";
 
     @Bean
-    public FirebaseApp init() throws IOException {
+    public FirebaseApp firebaseApp() {
+        String configStringValue = config.getConfigStringValue();
+        try (InputStream inputStream = new ByteArrayInputStream(configStringValue.getBytes(StandardCharsets.UTF_8))) {
+            List<FirebaseApp> apps = FirebaseApp.getApps();
 
-        ClassPathResource resource = new ClassPathResource(configPath);
+            if (Objects.isNull(apps) || apps.isEmpty()) {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(inputStream))
+                        .build();
+                FirebaseApp firebaseApp = FirebaseApp.initializeApp(options, firebaseAppName);
+                log.warn("FirebaseApp initialization complete");
+                return firebaseApp;
+            }
 
-        try (InputStream inputStream = resource.getInputStream()) {
-
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(inputStream))
-                    .build();
-
-            log.warn("FirebaseApp initialization complete");
-            return FirebaseApp.initializeApp(options);
+            return apps.get(0);
         } catch (Exception e) {
-            log.warn(e.getClass().toString());
             log.warn("FirebaseApp initialization failed");
             throw new NullPointerException();
         }
     }
 
     @Bean
-    public FirebaseMessaging initFirebaseMessaging(FirebaseApp firebaseApp) {
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
         return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
